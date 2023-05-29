c1df97e59a12b1152f431be5aade2d3acb4c29e3
vpaliok <viliuspaliokas@gmail.com>
2023-05-30 01:01:29 +0300

    Remove dtd and mode files

--- tools/musicxml_generator.dart
+++ tools/musicxml_generator.dart
@@ -8,48 +8,49 @@ const schemaFiles = [
   "xml.xsd",
   "xlink.xsd",
   "to31.xsl",
-  "sounds.dtd",
+  // "sounds.dtd",
   "to11.xsl",
   "to20.xsl",
   "to10.xsl",
-  "timewise.dtd",
-  "score.mod",
+  // "timewise.dtd",
+  // "score.mod",
   "to30.xsl",
   "sounds.xml",
   "sounds.xsd",
-  "direction.mod",
+  // "direction.mod",
   "musicxml.xsd",
   "opus.xsd",
-  "partwise.dtd",
+  // "partwise.dtd",
   "catalog.xml",
-  "barline.mod",
-  "container.dtd",
+  // "barline.mod",
+  // "container.dtd",
   "container.xsd",
   "parttime.xsl",
-  "note.mod",
-  "link.mod",
-  "midixml.dtd",
-  "common.mod",
+  // "note.mod",
+  // "link.mod",
+  // "midixml.dtd",
+  // "common.mod",
   "midixml.xsl",
-  "opus.dtd",
-  "layout.mod",
+  // "opus.dtd",
+  // "layout.mod",
   "isolat1.ent",
-  "attributes.mod",
-  "identity.mod",
+  // "attributes.mod",
+  // "identity.mod",
   "isolat2.ent",
 ];
 
 const repo = "https://raw.githubusercontent.com/w3c/musicxml/gh-pages/schema/";
 
 Future recordDate() async {
-  DateTime now = DateTime.now();
-  String formattedDate = '${now.year}-${now.month}-${now.day}';
+  String formattedDate = DateTime.now().toIso8601String();
 
   String filePath =
       './tools/raw_schemas/meta.txt'; // Replace with your desired file path
 
   File file = File(filePath);
-  await file.writeAsString(formattedDate);
+  await file.writeAsString("Downloaded at\n", mode: FileMode.writeOnly);
+
+  await file.writeAsString(formattedDate, mode: FileMode.writeOnlyAppend);
 
   print('Current date has been written to the $filePath: $formattedDate');
 }
@@ -62,15 +63,70 @@ Future download(String link, String name) {
           response.pipe(File(name).openWrite())));
 }
 
-main() async {
-  print('Downloading latest MusicXML specifications');
-  for (var file in schemaFiles) {
-    print('${schemaFiles.indexOf(file)} of ${schemaFiles.length}');
-    print('Downloading: $file');
-    await download(repo + file, './tools/raw_schemas/$file');
+
+/// Checks if a folder contains only the provided files.
+///
+/// Returns `true` if the folder contains only the provided files, otherwise `false`.
+bool checkFolderForOnlyFiles(String folderPath, List<String> files) {
+  Directory directory = Directory(folderPath);
+  List<String> folderFiles = directory.listSync()
+      .whereType<File>()
+      .map((entity) => entity.path.split('/').last)
+      .toList();
+
+  // Check if the number of files in the folder matches the expected number
+  if (folderFiles.length != files.length) {
+    return false;
   }
 
-  recordDate();
+  // Check if each file in the folder is in the provided list
+  for (final file in folderFiles) {
+    if (!files.contains(file)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/// Clears every file from provided [folderPath].
+///
+/// Returns nothing.
+void clearFolder(String folderPath) {
+  Directory directory = Directory(folderPath);
+
+  List<FileSystemEntity> files = directory.listSync();
+
+  for (FileSystemEntity file in files) {
+    if (file is File) {
+      file.deleteSync();
+    }
+  }
+}
+
+
+main(List<String> arguments) async {
+  const schemaFolder = './tools/raw_schemas';
+
+  // Check if the folder contains all the required files
+  bool hasAllFiles = checkFolderForOnlyFiles(schemaFolder, schemaFiles);
+
+  if (arguments.contains("force-read") || !hasAllFiles) {
+
+    print("Clearing existing files");
+
+    clearFolder(schemaFolder);
+
+    print('Downloading latest MusicXML specifications');
+
+    for (var file in schemaFiles) {
+      print('${schemaFiles.indexOf(file)} of ${schemaFiles.length}');
+      print('Downloading: $file');
+      await download(repo + file, '$schemaFolder/$file');
+    }
+
+    await recordDate();
+  }
 
   // print('generating');
   // await generateCode(
