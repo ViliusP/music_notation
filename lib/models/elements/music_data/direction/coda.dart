fac7ed87bf582e15fd498081afb21437cd3d638d
ViliusP <viliuspaliokas@gmail.com>
2023-06-12 17:29:42 +0300

    initial coda

---
+++ lib/models/elements/music_data/direction/coda.dart
@@ -0,0 +1,15 @@
+import 'package:music_notation/models/elements/music_data/direction/direction_type.dart';
+import 'package:music_notation/models/printing.dart';
+
+// /The coda type is the visual indicator of a coda sign. The exact glyph can be specified with the smufl attribute. A sound element is also needed to guide playback applications reliably.
+class Coda extends DirectionType {
+  PrintStyleAlign printStyleAlign;
+
+  String? id;
+
+  String? smufl;
+
+  Coda({
+    required this.printStyleAlign,
+  });
+}
