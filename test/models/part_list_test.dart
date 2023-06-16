8b1ac27b07bd045772630d92b52b6b47e6969731
ViliusP <viliuspaliokas@gmail.com>
2023-06-16 23:33:51 +0300

    tests

---
+++ test/models/part_list_test.dart
@@ -0,0 +1,43 @@
+import 'package:music_notation/src/models/elements/score/part_list.dart';
+import 'package:music_notation/src/models/elements/score/score_part.dart';
+import 'package:test/test.dart';
+import 'package:xml/xml.dart';
+
+void main() {
+  group('Part list parsing', () {
+    // https://www.w3.org/2021/06/musicxml40/musicxml-reference/examples/assess-and-player-elements/
+    test('Should parse part list from <assess> and <player> example correctly',
+        () {
+      String input = '''
+        <part-list>
+            <part-group number="1" type="start">
+              <group-symbol default-x="-5">bracket</group-symbol>
+              <group-barline>yes</group-barline>
+            </part-group>
+            <score-part id="P1">
+              <part-name>Piccolo</part-name>
+              <group>score</group>
+            </score-part>
+            <score-part id="P2">
+              <part-name>Oboe</part-name>
+              <group>score</group>
+            </score-part>
+            <score-part id="P3">
+              <part-name>Bass Clarinet</part-name>
+              <group>score</group>
+            </score-part>
+            <part-group number="1" type="stop"/>
+        </part-list>
+      ''';
+
+      var rootElement = XmlDocument.parse(input).rootElement;
+
+      var partList = PartList.fromXml(rootElement);
+
+      expect(partList.partGroups.length, 1);
+      expect(partList.additionalParts.length, 3);
+      expect(partList.additionalParts.whereType<ScorePart>().length, 2);
+      expect(partList.additionalParts.whereType<PartGroup>().length, 1);
+    });
+  });
+}
