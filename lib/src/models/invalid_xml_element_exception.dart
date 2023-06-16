b7826fbf3774fc7b12dcb468115a384b1c36dbf1
ViliusP <viliuspaliokas@gmail.com>
2023-06-16 23:53:20 +0300

    InvalidXmlElementException comment

--- lib/src/models/invalid_xml_element_exception.dart
+++ lib/src/models/invalid_xml_element_exception.dart
@@ -1,13 +1,21 @@
 import 'package:xml/xml.dart';
 
+/// Exception that is thrown when an invalid XmlElement is encountered
+/// while parsing the MusicXML data.
 class InvalidXmlElementException implements Exception {
+  /// A message that describes the error in detail.
   final String message;
+
+  /// The XmlElement which caused the exception to be thrown.
   final XmlElement xmlElement;
+
+  /// Creates an instance of the InvalidXmlElementException with an
+  /// error [message] and the offending [xmlElement].
   InvalidXmlElementException(this.message, this.xmlElement);
 
   @override
   String toString() {
-    return 'InvalidXmlElementAttributeException: $message';
+    return 'InvalidXmlElementException: $message';
   }
 }
 
