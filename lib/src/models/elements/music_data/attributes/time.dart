b549a26224779182745d21f9ea6411c27c201c4a
ViliusP <viliuspaliokas@gmail.com>
2023-06-28 21:02:32 +0300

    Finish senza misura parsing from xml

--- lib/src/models/elements/music_data/attributes/time.dart
+++ lib/src/models/elements/music_data/attributes/time.dart
@@ -1,29 +1,11 @@
-// 	<xs:complexType name="time">
-// 		<xs:annotation>
-// 			<xs:documentation>Time signatures are represented by the beats element for the numerator and the beat-type element for the denominator. The symbol attribute is used to indicate common and cut time symbols as well as a single number display. Multiple pairs of beat and beat-type elements are used for composite time signatures with multiple denominators, such as 2/4 + 3/8. A composite such as 3+2/8 requires only one beat/beat-type pair.
-
-// The print-object attribute allows a time signature to be specified but not printed, as is the case for excerpts from the middle of a score. The value is "yes" if not present. The optional number attribute refers to staff numbers within the part. If absent, the time signature applies to all staves in the part.</xs:documentation>
-// 		</xs:annotation>
-// 		<xs:choice>
-// 			<xs:sequence>
-// 				<xs:group ref="time-signature" maxOccurs="unbounded"/>
-// 				<xs:element name="interchangeable" type="interchangeable" minOccurs="0"/>
-// 			</xs:sequence>
-// 			<xs:element name="senza-misura" type="xs:string">
-// 				<xs:annotation>
-// 					<xs:documentation>A senza-misura element explicitly indicates that no time signature is present. The optional element content indicates the symbol to be used, if any, such as an X. The time element's symbol attribute is not used when a senza-misura element is present.</xs:documentation>
-// 				</xs:annotation>
-// 			</xs:element>
-// 		</xs:choice>
-// 		<xs:attribute name="number" type="staff-number"/>
-// 		<xs:attribute name="symbol" type="time-symbol"/>
-// 		<xs:attribute name="separator" type="time-separator"/>
-// 		<xs:attributeGroup ref="print-style-align"/>
-// 		<xs:attributeGroup ref="print-object"/>
-// 		<xs:attributeGroup ref="optional-unique-id"/>
-// 	</xs:complexType>
-
+import 'package:collection/collection.dart';
+import 'package:music_notation/src/models/exceptions.dart';
 import 'package:music_notation/src/models/printing.dart';
+import 'package:music_notation/src/models/utilities/case_transformers.dart';
+import 'package:music_notation/src/models/utilities/common_attributes.dart';
+import 'package:music_notation/src/models/utilities/type_parsers.dart';
+import 'package:music_notation/src/models/utilities/xml_sequence_validator.dart';
+import 'package:xml/xml.dart';
 
 /// Time signatures are represented by the beats element for the numerator and
 /// the beat-type element for the denominator.
@@ -38,14 +20,14 @@ import 'package:music_notation/src/models/printing.dart';
 ///
 /// The optional number attribute refers to staff numbers within the part.
 /// If absent, the time signature applies to all staves in the part.
-abstract class Time {
+sealed class Time {
   // ------------------------- //
   // ------ Attributes ------- //
   // ------------------------- //
 
   /// Allows a time signature to apply to only the specified staff in the part.
   /// If absent, the time signature applies to all staves in the part.
-  int number;
+  int? number;
 
   /// Indicates how to display a time signature,
   /// such as by using common and cut time symbols or a single number display.
@@ -61,13 +43,91 @@ abstract class Time {
   String? id;
 
   Time({
-    required this.number,
+    this.number,
     this.symbol = TimeSymbol.normal,
     this.separator = TimeSeparator.none,
-    required this.printStyleAlign,
-    required this.printObject,
+    this.printStyleAlign = const PrintStyleAlign.empty(),
+    this.printObject = true,
     this.id,
   });
+
+  static int? _numberFromXml(XmlElement xmlElement) {
+    String? rawNumber = xmlElement.getAttribute("number");
+
+    if (rawNumber == null) {
+      return null;
+    }
+
+    int? number = int.tryParse(rawNumber);
+
+    if (number == null || number < 1) {
+      throw MusicXmlFormatException(
+        message: "'number' in 'time' must be positive integer",
+        xmlElement: xmlElement,
+      );
+    }
+
+    return number;
+  }
+
+  static TimeSeparator _seperatorFromXml(XmlElement xmlElement) {
+    String? rawSeparator = xmlElement.getAttribute("separator");
+
+    if (rawSeparator == null) {
+      return TimeSeparator.none;
+    }
+
+    TimeSeparator? separator = TimeSeparator.fromString(rawSeparator);
+
+    if (separator == null) {
+      throw MusicXmlTypeException(
+        message: "'separator' in 'time' must be time-seperator",
+        xmlElement: xmlElement,
+      );
+    }
+
+    return separator;
+  }
+
+  static TimeSymbol _symbolFromXml(XmlElement xmlElement) {
+    String? rawValue = xmlElement.getAttribute("symbol");
+
+    if (rawValue == null) {
+      return TimeSymbol.normal;
+    }
+
+    TimeSymbol? value = TimeSymbol.fromString(rawValue);
+
+    if (value == null) {
+      throw MusicXmlTypeException(
+        message: "'symbol' in 'time' must be time-symbol",
+        xmlElement: xmlElement,
+      );
+    }
+
+    return value;
+  }
+
+  factory Time.fromXml(XmlElement xmlElement) {
+    try {
+      return TimeBeat.fromXml(xmlElement);
+    } on XmlSequenceException catch (_) {
+    } catch (e) {
+      rethrow;
+    }
+
+    try {
+      return SenzaMisura.fromXml(xmlElement);
+    } on XmlSequenceException catch (_) {
+    } catch (e) {
+      rethrow;
+    }
+
+    throw XmlElementContentException(
+      message: "Invalid 'time' element content",
+      xmlElement: xmlElement,
+    );
+  }
 }
 
 class TimeBeat extends Time {
@@ -134,9 +194,8 @@ enum TimeRelation {
   hyphen;
 }
 
-/// A senza-misura element explicitly indicates that no time signature is present.
-///
-/// The optional element content indicates the symbol to be used, if any, such as an X.
+/// Explicitly indicates that no time signature is present.The optional element
+/// content indicates the symbol to be used, if any, such as an X.
 ///
 /// The time element's symbol attribute is not used when a senza-misura element is present.
 class SenzaMisura extends Time {
@@ -144,54 +203,96 @@ class SenzaMisura extends Time {
   // ------   Content   ------ //
   // ------------------------- //
 
-  String? content;
+  /// Indicates the symbol to be used, if any, such as an X.
+  String content;
 
   SenzaMisura({
-    required super.number,
-    required super.printStyleAlign,
-    required super.printObject,
+    this.content = "",
+    super.number,
+    super.symbol,
+    super.separator,
+    super.printStyleAlign,
+    super.printObject,
+    super.id,
   });
+
+  // Field(s): quantifier
+  static const Map<dynamic, XmlQuantifier> _xmlExpectedOrder = {
+    'senza-misura': XmlQuantifier.required,
+  };
+
+  factory SenzaMisura.fromXml(XmlElement xmlElement) {
+    validateSequence(xmlElement, _xmlExpectedOrder);
+
+    XmlElement? senzaMisuraElement = xmlElement.getElement("senza-misura");
+    validateTextContent(senzaMisuraElement);
+
+    bool? printObject = YesNo.fromXml(
+      xmlElement,
+      CommonAttributes.printObject,
+    );
+
+    return SenzaMisura(
+      content: senzaMisuraElement!.innerText,
+      number: Time._numberFromXml(xmlElement),
+      symbol: Time._symbolFromXml(xmlElement),
+      separator: Time._seperatorFromXml(xmlElement),
+      printStyleAlign: PrintStyleAlign.fromXml(xmlElement),
+      printObject: printObject ?? true,
+      id: xmlElement.getAttribute("id"),
+    );
+  }
 }
 
-/// The time-symbol type indicates how to display a time signature.
-///
-/// The normal value is the usual fractional display, and is the implied symbol type if none is specified.
-///
-/// Other options are the common and cut time symbols,
-/// as well as a single number with an implied denominator.
-///
-/// The note symbol indicates that the beat-type should be represented
-/// with the corresponding downstem note rather than a number.
-///
-/// The dotted-note symbol indicates that the beat-type should be represented
-/// with a dotted downstem note that corresponds to three times the beat-type value,
-/// and a numerator that is one third the beats value.
+/// Indicates how to display a time signature.
 enum TimeSymbol {
+  /// Common time symbol.
   common,
+
+  /// Cut time symbol.
   cut,
+
+  /// Single number with an implied denominator.
   singleNumber,
+
+  /// Indicates that the beat-type should be represented with the corresponding
+  /// downstem note rather than a number.
   note,
+
+  /// Indicates that the beat-type should be represented with a dotted downstem
+  /// note that corresponds to three times the beat-type value, and a numerator
+  /// that is one third the beats value.
   dottedNote,
+
+  /// Usual fractional display, and is the implied symbol type if none is specified.
   normal;
+
+  static TimeSymbol? fromString(String value) {
+    return values.firstWhereOrNull(
+      (element) => element.name == hyphenToCamelCase(value),
+    );
+  }
 }
 
-/// The time-separator type indicates how to display the arrangement between
-/// the beats and beat-type values in a time signature.
-///
-/// The default value is none.
-///
-/// The horizontal, diagonal, and vertical values represent
-/// horizontal, diagonal lower-left to upper-right, and vertical lines respectively.
-///
-/// For these values, the beats and beat-type values are arranged on either side of the separator line.
-///
-/// The none value represents no separator with the beats and beat-type arranged vertically.
-///
-/// The adjacent value represents no separator with the beats and beat-type arranged horizontally.
+/// Indicates how to display the arrangement between the beats and beat-type
+/// values in a time signature. The default value is none.
 enum TimeSeparator {
+  /// Represents no separator with the beats and beat-type arranged vertically.
   none,
+
+  /// Horizontal line with the beats and beat-type arranged on either side of the separator line.
   horizontal,
+
+  /// Diagonal line with the beats and beat-type arranged on either side of the separator line.
   diagonal,
+
+  /// Vertical line with the beats and beat-type arranged on either side of the separator line.
   vertical,
+
+  /// Represents no separator with the beats and beat-type arranged horizontally.
   adjacent;
+
+  static TimeSeparator? fromString(String value) {
+    return values.firstWhereOrNull((element) => element.name == value);
+  }
 }
--- test/models/elements/music_data/attributes/time_test.dart
+++ test/models/elements/music_data/attributes/time_test.dart
@@ -1,1 +1,114 @@
+import 'package:music_notation/src/models/elements/music_data/attributes/time.dart';
+import 'package:music_notation/src/models/exceptions.dart';
+import 'package:test/test.dart';
+import 'package:xml/xml.dart';
 
+void main() {
+  group("Time beat", () {});
+  group("Interchangeable", () {});
+
+  group("Senza Misura", () {
+    test("should parse '<senza-misura>' example", () {
+      String input = """
+        <time>
+          <senza-misura/>
+        </time>
+      """;
+
+      Time time = Time.fromXml(
+        XmlDocument.parse(input).rootElement,
+      );
+
+      expect(time.id, isNull);
+      expect(time.number, isNull);
+      expect(time.printObject, true);
+      expect(time.separator, TimeSeparator.none);
+      expect(time.symbol, TimeSymbol.normal);
+      expect(time, isA<SenzaMisura>());
+
+      SenzaMisura senzaMisura = time as SenzaMisura;
+      expect(senzaMisura.content, isEmpty);
+    });
+    test("should parse with 'X' content", () {
+      String input = """
+        <time>
+          <senza-misura>X</senza-misura>
+        </time>
+      """;
+
+      Time time = Time.fromXml(
+        XmlDocument.parse(input).rootElement,
+      );
+
+      expect(time.id, isNull);
+      expect(time.number, isNull);
+      expect(time.printObject, true);
+      expect(time.separator, TimeSeparator.none);
+      expect(time.symbol, TimeSymbol.normal);
+      expect(time, isA<SenzaMisura>());
+
+      SenzaMisura senzaMisura = time as SenzaMisura;
+      expect(senzaMisura.content, "X");
+    });
+    test("parsing should throw on invalid print object value", () {
+      String input = """
+        <time print-object="foo">
+          <senza-misura >X</senza-misura>
+        </time>
+      """;
+
+      expect(
+        () => Time.fromXml(XmlDocument.parse(input).rootElement),
+        throwsA(isA<MusicXmlTypeException>()),
+      );
+    });
+    test("parsing should throw on invalid content", () {
+      String input = """
+        <time print-object="no">
+          <senza-misura ><foo></foo></senza-misura>
+        </time>
+      """;
+
+      expect(
+        () => Time.fromXml(XmlDocument.parse(input).rootElement),
+        throwsA(isA<XmlElementContentException>()),
+      );
+    });
+    test("parsing should throw on invalid number", () {
+      String input = """
+        <time number="foo" print-object="no">
+          <senza-misura/>
+        </time>
+      """;
+
+      expect(
+        () => Time.fromXml(XmlDocument.parse(input).rootElement),
+        throwsA(isA<MusicXmlFormatException>()),
+      );
+    });
+    test("parsing should throw on invalid symbol", () {
+      String input = """
+        <time symbol="foo" print-object="no">
+          <senza-misura/>
+        </time>
+      """;
+
+      expect(
+        () => Time.fromXml(XmlDocument.parse(input).rootElement),
+        throwsA(isA<MusicXmlTypeException>()),
+      );
+    });
+    test("parsing should throw on invalid separator", () {
+      String input = """
+        <time separator="foo" print-object="no">
+          <senza-misura/>
+        </time>
+      """;
+
+      expect(
+        () => Time.fromXml(XmlDocument.parse(input).rootElement),
+        throwsA(isA<MusicXmlTypeException>()),
+      );
+    });
+  });
+}
