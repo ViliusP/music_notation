b623d4033276c96086c996948b5b00ae832ce313
ViliusP <viliuspaliokas@gmail.com>
2023-06-30 13:47:00 +0300

    Add time-modification parsing and tests

--- lib/src/models/elements/music_data/note/time_modification.dart
+++ lib/src/models/elements/music_data/note/time_modification.dart
@@ -1,93 +1,115 @@
 import 'package:music_notation/src/models/elements/music_data/note/note_type.dart';
 import 'package:music_notation/src/models/exceptions.dart';
+import 'package:music_notation/src/models/utilities/xml_sequence_validator.dart';
 import 'package:xml/xml.dart';
 
-/// Time modification indicates tuplets, double-note tremolos, and other durational changes.
+/// The [TimeModification] class represents durational changes in MusicXML,
+/// including tuplets and double-note tremolos.
 ///
-/// A <time-modification> element shows how the cumulative, sounding effect of tuplets and
-/// double-note tremolos compare to the written note type represented by the <type> and <dot> elements.
+/// It shows the ratio between the actual notes being played and the number of
+/// notes typically played for the same duration. This is often used to indicate
+/// tuplets, where for example, three notes can be played in the time of two.
 ///
-/// Nested tuplets and other notations that use more detailed information need both the <time-modification> and <tuplet> elements to be represented accurately.
+/// If the written note type of the tuplet is different from the surrounding notes
+/// (e.g., an eighth-note triplet within quarter notes), this can be specified
+/// with the [normalType] and [normalDots] properties.
+///
+/// More complex notations may require both a [TimeModification] and
+/// a tuplet description for accurate representation.
+///
+/// For more details, refer to the MusicXML specification:
+/// [The <time-modification> element | MusicXML 4.0](https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/time-modification/).
 class TimeModification {
-  /// The actual-notes element describes how many notes are played in the time usually occupied by the number in the normal-notes element.
-  ///
-  /// nonNegativeInteger data type.
-  ///
-  /// Required.
+  /// The number of notes that are played in the time typically taken by
+  /// [normalNotes] number of notes.
   int actualNotes;
 
-  /// The normal-notes element describes how many notes are usually played in the time occupied by the number in the actual-notes element.
-  ///
-  /// nonNegativeInteger data type.
-  ///
-  /// Required.
+  /// The number of notes that are typically played in the time taken by
+  /// [actualNotes] number of notes.
   int normalNotes;
 
-  /// If the type associated with the number in the normal-notes element is different than the current note type (e.g., a quarter note within an eighth note triplet),
-  /// then the normal-notes type (e.g. eighth) is specified in the normal-type and normal-dot elements.
+  /// Specifies the written note type when different from the current note type.
+  ///
+  /// For example, it would be "eighth" for an eighth-note triplet within a quarter note context.
   NoteTypeValue? normalType;
 
-  /// The normal-dot element is used to specify dotted normal tuplet types.
+  /// Specifies the number of dots for the written note type.
   ///
-  /// This property cannot exists without [normalType].
-  int? normalDots;
+  /// This property can only exist if [normalType] is specified.
+  int normalDots;
 
+  /// Constructs a new [TimeModification] with the given values.
+  ///
+  /// All parameters except [actualNotes] and [normalNotes] are optional.
   TimeModification({
     required this.actualNotes,
     required this.normalNotes,
     this.normalType,
-    this.normalDots,
+    this.normalDots = 0,
   });
 
+  // Specifies the expected order of xml elements.
+  static const Map<dynamic, XmlQuantifier> _xmlExpectedOrder = {
+    'actual-notes': XmlQuantifier.required,
+    'normal-notes': XmlQuantifier.required,
+    {
+      'normal-type': XmlQuantifier.required,
+      'normal-dot': XmlQuantifier.zeroOrMore,
+    }: XmlQuantifier.optional,
+  };
+
+  /// Constructs a new [TimeModification] instance from an [XmlElement].
+  ///
+  /// Validates the order and content of the xml elements.
+  /// If [xmlElement] content sequence is in invalid order, it will throw a [XmlSequenceException].
+  /// If any values are invalid, it will throw a [MusicXmlFormatException] or [MusicXmlTypeException].
   factory TimeModification.fromXml(XmlElement xmlElement) {
-    int? actualNotes = int.tryParse(
-      xmlElement.getElement("actual-notes")?.innerText ?? "",
-    );
+    validateSequence(xmlElement, _xmlExpectedOrder);
 
-    if (actualNotes == null) {
-      throw XmlElementContentException(
-        message: "actual-notes value is missing",
-        xmlElement: xmlElement,
+    XmlElement actualNotesElement = xmlElement.getElement("actual-notes")!;
+    validateTextContent(actualNotesElement);
+    int? actualNotes = int.tryParse(actualNotesElement.innerText);
+    if (actualNotes == null || actualNotes < 0) {
+      throw MusicXmlFormatException(
+        message: "actual-notes value is not valid non negative integer",
+        xmlElement: actualNotesElement,
+        source: actualNotesElement.innerText,
       );
     }
 
-    int? normalNotes = int.tryParse(
-      xmlElement.getElement("normal-notes")?.innerText ?? "",
-    );
-
-    if (normalNotes == null) {
-      throw XmlElementContentException(
-        message: "normal-notes value is missing",
-        xmlElement: xmlElement,
+    XmlElement normalNotesElement = xmlElement.getElement("normal-notes")!;
+    validateTextContent(normalNotesElement);
+    int? normalNotes = int.tryParse(normalNotesElement.innerText);
+    if (normalNotes == null || normalNotes < 0) {
+      throw MusicXmlFormatException(
+        message: "normal-notes value is not valid non negative integer",
+        xmlElement: normalNotesElement,
+        source: normalNotesElement.innerText,
       );
     }
 
-    NoteTypeValue? maybeNormalType = NoteTypeValue.fromString(
-      xmlElement.getElement("normal-type")?.innerText ?? "",
+    XmlElement? normalTypeElement = xmlElement.getElement("normal-type");
+    validateTextContent(normalTypeElement);
+    NoteTypeValue? normalType = NoteTypeValue.fromString(
+      normalTypeElement?.innerText ?? "",
     );
+    if (normalTypeElement != null && normalType == null) {
+      throw MusicXmlTypeException(
+        message: "normal type value is not valid note type value",
+        xmlElement: normalTypeElement,
+      );
+    }
 
-    int? normalDots;
-
-    if (maybeNormalType != null) {
-      normalDots = _calculateDots(xmlElement);
+    int normalDots = 0;
+    if (normalType != null) {
+      normalDots = xmlElement.findElements("normal-dot").length;
     }
 
     return TimeModification(
       actualNotes: actualNotes,
       normalNotes: normalNotes,
-      normalType: maybeNormalType,
+      normalType: normalType,
       normalDots: normalDots,
     );
-  }
-
-  static int _calculateDots(XmlElement xmlElement) {
-    final normalType = xmlElement.getElement("normal-type");
-    var sibling = normalType?.nextElementSibling;
-    var count = 0;
-    while (sibling != null && sibling.name.local == 'normal-dot') {
-      count++;
-      sibling = sibling.nextElementSibling;
-    }
-    return count;
   }
 }
