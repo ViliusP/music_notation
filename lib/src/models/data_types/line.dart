4d07293e5ee651d7bfb536435c188325876b2524
ViliusP <viliuspaliokas@gmail.com>
2023-07-06 16:50:30 +0300

    Add DashedFormatting and LineType parsing from xml

--- lib/src/models/data_types/line.dart
+++ lib/src/models/data_types/line.dart
@@ -1,19 +1,19 @@
+import 'package:collection/collection.dart';
+import 'package:music_notation/src/models/exceptions.dart';
+import 'package:music_notation/src/models/utilities/type_parsers.dart';
 import 'package:xml/xml.dart';
 
-/// Attribute group.
+/// [DashedFormatting] is an attribute group class representing the length of
+/// dashes and spaces in a dashed line in MusicXML.
 ///
-/// The dashed-formatting entity represents the length of dashes and spaces in a
-/// dashed line. Both the [dashlength] and [spacelength] attributes are
-/// represented in tenths.
-///
-/// These attributes are ignored if the corresponding `line type` attribute is
-/// not dashed.
+/// Both the [dashLength] and [spaceLength] attributes are represented in tenths.
+/// These attributes are ignored if the corresponding `line type` attribute is not dashed.
 class DashedFormatting {
   /// The length of dashes in a dashed line.
-  final int? dashLength;
+  final double? dashLength;
 
   /// The length of spaces in a dashed line.
-  final int? spaceLength;
+  final double? spaceLength;
 
   const DashedFormatting({
     this.dashLength,
@@ -24,15 +24,63 @@ class DashedFormatting {
       : dashLength = null,
         spaceLength = null;
 
+  /// Constructs a [DashedFormatting] instance from a [XmlElement].
+  ///
+  /// Example:
+  /// ```dart
+  /// var dashedFormatting = DashedFormatting.fromXml(parse('<line dash-length="4" space-length="2"/>'));
+  /// print(dashedFormatting.dashLength);  // 4
+  /// print(dashedFormatting.spaceLength); // 2
+  /// ```
   factory DashedFormatting.fromXml(XmlElement xmlElement) {
-    return const DashedFormatting();
+    return DashedFormatting(
+      dashLength: Decimal.fromXml(xmlElement, "dash-length", false),
+      spaceLength: Decimal.fromXml(xmlElement, "space-length", false),
+    );
   }
 }
 
-/// The line-type type distinguishes between solid, dashed, dotted, and wavy lines.
+/// [LineType] is an enum that distinguishes between solid, dashed, dotted,
+/// and wavy lines in a MusicXML document.
 enum LineType {
   solid,
   dashed,
   dotted,
   wavy;
+
+  /// Returns the corresponding [LineType] value for a given string.
+  ///
+  /// Example:
+  /// ```dart
+  /// var lineType = LineType.fromString('dashed');
+  /// print(lineType == LineType.dashed);  // true
+  /// ```
+  static LineType? fromString(String value) =>
+      values.firstWhereOrNull((element) => element.name == value);
+
+  /// Constructs a [LineType] instance from a [XmlElement].
+  ///
+  /// If the provided [xmlElement] has a `line-type` attribute, it will be parsed
+  /// and used to create the [LineType] instance.
+  ///
+  /// If the `line-type` attribute is absent, this method will return `null`.
+  ///
+  /// If the `line-type` attribute is present but does not match valid
+  /// [LineType] value, this method will throw a [MusicXmlTypeException].
+  static LineType? fromXml(XmlElement xmlElement) {
+    var lineTypeAttribute = xmlElement.getAttribute("line-type");
+    if (lineTypeAttribute == null) {
+      return null;
+    }
+    var lineType = fromString(lineTypeAttribute);
+
+    if (lineType == null) {
+      throw MusicXmlTypeException(
+        message:
+            "'line-type' attribute in ${xmlElement.localName} is not valid line type",
+        xmlElement: xmlElement,
+      );
+    }
+    return lineType;
+  }
 }
