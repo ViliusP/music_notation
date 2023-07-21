e61eb83e524894509d1d1bbbaccb529aabec1e5c
ViliusP <viliuspaliokas@gmail.com>
2023-07-21 15:50:55 +0300

    Add context offset helper methods

--- lib/src/notation_painter/staff_painter_context.dart
+++ lib/src/notation_painter/staff_painter_context.dart
@@ -3,7 +3,17 @@ import 'dart:ui';
 import 'package:music_notation/src/models/elements/score/part.dart';
 
 class StaffPainterContext {
-  Offset offset = const Offset(0, 0);
+  Offset _offset = const Offset(0, 0);
+  Offset get offset => _offset;
+
+  void moveX(double x) {
+    _offset += Offset(x, 0);
+  }
+
+  void moveY(double y) {
+    _offset += Offset(0, y);
+  }
+
   Part? currentPart;
   Measure? currentMeasure;
 
