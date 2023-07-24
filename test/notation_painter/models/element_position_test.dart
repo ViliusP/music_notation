0ee2e9ab9bf65e65a04f995a347f61c09a0ba15a
ViliusP <viliuspaliokas@gmail.com>
2023-07-24 11:07:11 +0300

    Add consts to element position tests

--- test/notation_painter/models/element_position_test.dart
+++ test/notation_painter/models/element_position_test.dart
@@ -6,90 +6,90 @@ void main() {
   group('Operators', () {
     // ==
     test("true: G4 == G4  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.G, octave: 4);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.G, octave: 4);
 
       expect(a == b, isTrue);
     });
     test("false: G4 == C9  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.C, octave: 9);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.C, octave: 9);
 
       expect(a == b, isFalse);
     });
     // <
     test("false: G4 < G4  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.G, octave: 4);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.G, octave: 4);
 
       expect(a < b, isFalse);
     });
     test("true: G5 < D6  ", () {
-      var a = ElementPosition(step: Step.G, octave: 5);
-      var b = ElementPosition(step: Step.D, octave: 6);
+      var a = const ElementPosition(step: Step.G, octave: 5);
+      var b = const ElementPosition(step: Step.D, octave: 6);
 
       expect(a < b, isTrue);
     });
     test("false: G4 < C3  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.C, octave: 3);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.C, octave: 3);
 
       expect(a < b, isFalse);
     });
     // >
     test("false: G4 > G4  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.G, octave: 4);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.G, octave: 4);
 
       expect(a > b, isFalse);
     });
     test("true: G5 > D6  ", () {
-      var a = ElementPosition(step: Step.G, octave: 5);
-      var b = ElementPosition(step: Step.D, octave: 6);
+      var a = const ElementPosition(step: Step.G, octave: 5);
+      var b = const ElementPosition(step: Step.D, octave: 6);
 
       expect(a > b, isFalse);
     });
     test("false: G4 > C3  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.C, octave: 3);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.C, octave: 3);
 
       expect(a > b, isTrue);
     });
     // >=
     test("true: G4 >= G4  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.G, octave: 4);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.G, octave: 4);
 
       expect(a >= b, true);
     });
     test("true: G5 >= D6  ", () {
-      var a = ElementPosition(step: Step.G, octave: 5);
-      var b = ElementPosition(step: Step.D, octave: 6);
+      var a = const ElementPosition(step: Step.G, octave: 5);
+      var b = const ElementPosition(step: Step.D, octave: 6);
 
       expect(a >= b, isFalse);
     });
     test("false: G4 > C3  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.C, octave: 3);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.C, octave: 3);
 
       expect(a >= b, isTrue);
     });
     // <=
     test("true: G4 <= G4  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.G, octave: 4);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.G, octave: 4);
 
       expect(a <= b, true);
     });
     test("true: G5 >= D6  ", () {
-      var a = ElementPosition(step: Step.G, octave: 5);
-      var b = ElementPosition(step: Step.D, octave: 6);
+      var a = const ElementPosition(step: Step.G, octave: 5);
+      var b = const ElementPosition(step: Step.D, octave: 6);
 
       expect(a <= b, isTrue);
     });
     test("false: G4 > C3  ", () {
-      var a = ElementPosition(step: Step.G, octave: 4);
-      var b = ElementPosition(step: Step.C, octave: 3);
+      var a = const ElementPosition(step: Step.G, octave: 4);
+      var b = const ElementPosition(step: Step.C, octave: 3);
 
       expect(a <= b, isFalse);
     });
