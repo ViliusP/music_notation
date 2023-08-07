87081fa2fdbe3abb76dcd23c37f84e38fec74e2f
ViliusP <viliuspaliokas@gmail.com>
2023-08-07 13:58:15 +0300

    Paint initial staff lines in stack

--- lib/src/notation_painter/music_notation_canvas.dart
+++ lib/src/notation_painter/music_notation_canvas.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/widgets.dart';
+import 'package:music_notation/src/models/elements/music_data/print.dart';
 import 'package:music_notation/src/models/elements/score/score.dart';
+import 'package:music_notation/src/notation_painter/music_grid.dart';
 import 'package:music_notation/src/notation_painter/staff_lines_painter.dart';
 
 class MusicNotationCanvas extends StatelessWidget {
   final ScorePartwise scorePartwise;
+  NotationGrid get notationGrid => NotationGrid.fromScoreParts(
+        scorePartwise.parts,
+      );
 
   const MusicNotationCanvas({
     super.key,
@@ -12,11 +17,34 @@ class MusicNotationCanvas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: ScorePartwisePainter(
-        score: scorePartwise,
-      ),
-      size: const Size(double.infinity, double.infinity),
+    List<Widget> staffs = [];
+    List<double> staffDistances = [];
+    for (int i = 0; i < notationGrid.data.rowCount; i++) {
+      double staffDistance = 0;
+      // ...some calculations.
+      staffDistance = i * 100;
+
+      staffDistances.add(staffDistance);
+      double staffLength = 0;
+      // ...some calculations.
+      staffLength = 500;
+
+      var staffLinePainter = StaffLinesPainter(staffLength);
+
+      staffs.add(
+        Positioned(
+          top: staffDistances[i],
+          child: CustomPaint(
+            size: staffLinePainter.size,
+            painter: staffLinePainter,
+          ),
+        ),
+      );
+    }
+    return Stack(
+      children: [
+        ...staffs,
+      ],
     );
   }
 }
