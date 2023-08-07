e71892f616a50caff16ef02d65df1486e771a55d
ViliusP <viliuspaliokas@gmail.com>
2023-08-07 16:43:08 +0300

    Implement intial stacked notation painting

---
+++ lib/src/notation_painter/measure.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+import 'package:music_notation/src/notation_painter/music_grid.dart';
+
+class Measure extends StatelessWidget {
+  final MeasureSequence sequence;
+
+  const Measure({super.key, required this.sequence});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [],
+    );
+  }
+}
--- lib/src/notation_painter/music_notation_canvas.dart
+++ lib/src/notation_painter/music_notation_canvas.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/widgets.dart';
-import 'package:music_notation/src/models/elements/music_data/print.dart';
 import 'package:music_notation/src/models/elements/score/score.dart';
+import 'package:music_notation/src/notation_painter/measure.dart';
 import 'package:music_notation/src/notation_painter/music_grid.dart';
+import 'package:music_notation/src/notation_painter/painters/barline_painter.dart';
 import 'package:music_notation/src/notation_painter/staff_lines_painter.dart';
+import 'package:music_notation/src/notation_painter/staff_painter_context.dart';
 
 class MusicNotationCanvas extends StatelessWidget {
   final ScorePartwise scorePartwise;
-  NotationGrid get notationGrid => NotationGrid.fromScoreParts(
+  NotationGrid get grid => NotationGrid.fromScoreParts(
         scorePartwise.parts,
       );
 
@@ -17,19 +19,57 @@ class MusicNotationCanvas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> staffs = [];
+    var context = StaffPainterContext();
+
+    var staffs = <Widget>[];
+    var barlines = <Widget>[];
+    var measures = <Widget>[];
+
     List<double> staffDistances = [];
-    for (int i = 0; i < notationGrid.data.rowCount; i++) {
-      double staffDistance = 0;
-      // ...some calculations.
-      staffDistance = i * 100;
+    for (int i = 0; i < grid.data.rowCount; i++) {
+      staffDistances.add(context.offset.dy);
+      // double staffLength = 0;
 
-      staffDistances.add(staffDistance);
-      double staffLength = 0;
-      // ...some calculations.
-      staffLength = 500;
+      var barlinePainter = BarlinePainter();
 
-      var staffLinePainter = StaffLinesPainter(staffLength);
+      barlines.add(
+        Positioned(
+          top: context.offset.dy,
+          left: context.offset.dx,
+          child: CustomPaint(
+            size: barlinePainter.size,
+            painter: barlinePainter,
+          ),
+        ),
+      );
+      context.moveX(12);
+
+      // Iterating throug measures/part.
+      for (var j = 0; j < grid.data.columnCount; j++) {
+        var measureSequence = grid.data.getValue(i, j);
+        measures.add(
+          Positioned(
+            top: context.offset.dy,
+            left: context.offset.dx,
+            child: Measure(sequence: measureSequence),
+          ),
+        );
+        barlines.add(
+          Positioned(
+            top: context.offset.dy,
+            left: context.offset.dx,
+            child: CustomPaint(
+              size: barlinePainter.size,
+              painter: barlinePainter,
+            ),
+          ),
+        );
+        if (j != grid.data.columnCount - 1) {
+          context.moveX(12);
+        }
+      }
+
+      var staffLinePainter = StaffLinesPainter(context.offset.dx);
 
       staffs.add(
         Positioned(
@@ -40,10 +80,14 @@ class MusicNotationCanvas extends StatelessWidget {
           ),
         ),
       );
+      context.resetX();
+      context.moveY(120);
     }
     return Stack(
+      fit: StackFit.expand,
       children: [
         ...staffs,
+        ...barlines,
       ],
     );
   }
