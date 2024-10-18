431f6ab258d2c2a32b94fac3f727d135ae8c2983
ViliusP <viliuspaliokas@gmail.com>
2024-10-18 21:24:30 +0300

    Add stems for notes on ledger lines example

--- example/lib/layout/notation_examples.dart
+++ example/lib/layout/notation_examples.dart
@@ -11,6 +11,10 @@ enum NotationExample {
     displayName: "Scale",
     path: "assets/scores/scale.xml",
   ),
+  stemsLedgerLines(
+    displayName: "Stems for notes on ledger lines",
+    path: "assets/scores/ledger_line_notes_stems.musicxml",
+  ),
   apresUnReve(
     displayName: "Tutorial: Après un rêve",
     path: "assets/scores/après_un_rêve.xml",
