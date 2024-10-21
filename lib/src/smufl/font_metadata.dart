95ca0d3be6a67f5558d4f1b296c89966ffb4fde9
ViliusP <viliuspaliokas@gmail.com>
2024-10-21 12:11:10 +0300

    Add AnchorField enumeration

--- lib/src/smufl/font_metadata.dart
+++ lib/src/smufl/font_metadata.dart
@@ -12,100 +12,100 @@ class EngravingDefaults {
   final List<String> textFontFamily;
 
   /// The thickness of the line used for the shaft of an arrow.
-  final double arrowShaftThickness;
+  final double? arrowShaftThickness;
 
   /// The default distance between multiple thin barlines when locked together,
   /// e.g. between two thin barlines making a double barline, measured from
   /// the right-hand edge of the left barline to the left-hand edge of the right barline.
-  final double barlineSeparation;
+  final double? barlineSeparation;
 
   /// The default distance between a pair of thin and thick barlines when
   /// locked together, e.g. between the thin and thick barlines making a final
   /// barline, or between the thick and thin barlines making a start repeat barline.
-  final double thinThickBarlineSeparation;
+  final double? thinThickBarlineSeparation;
 
   /// The distance between the inner edge of the primary and outer edge of subsequent secondary beams.
-  final double beamSpacing;
+  final double? beamSpacing;
 
   /// The thickness of a beam
-  final double beamThickness;
+  final double? beamThickness;
 
   /// The thickness of the vertical line of a bracket grouping staves together.
-  final double bracketThickness;
+  final double? bracketThickness;
 
   /// The length of the dashes to be used in a dashed barline.
-  final double dashedBarlineDashLength;
+  final double? dashedBarlineDashLength;
 
   /// The length of the gap between dashes in a dashed barline.
-  final double dashedBarlineGapLength;
+  final double? dashedBarlineGapLength;
 
   /// The thickness of a dashed barline.
-  final double dashedBarlineThickness;
+  final double? dashedBarlineThickness;
 
   /// The thickness of a crescendo/diminuendo hairpin.
-  final double hairpinThickness;
+  final double? hairpinThickness;
 
   /// The amount by which a ledger line should extend either side of a notehead,
   /// scaled proportionally with the notehead's size, e.g. when scaled down as a grace note
-  final double ledgerLineExtension;
+  final double? ledgerLineExtension;
 
   /// The thickness of a ledger line (normally somewhat thicker than a staff line)
-  final double ledgerLineThickness;
+  final double? ledgerLineThickness;
 
   /// The thickness of the lyric extension line to indicate a melisma in vocal music.
-  final double lyricLineThickness;
+  final double? lyricLineThickness;
 
   /// The thickness of the dashed line used for an octave line.
-  final double octaveLineThickness;
+  final double? octaveLineThickness;
 
   /// The thickness of the line used for piano pedaling.
-  final double pedalLineThickness;
+  final double? pedalLineThickness;
 
   /// The default horizontal distance between the dots and the inner barline of
   /// a repeat barline, measured from the edge of the dots to the edge of the barline.
-  final double repeatBarlineDotSeparation;
+  final double? repeatBarlineDotSeparation;
 
   /// The thickness of the brackets drawn to indicate repeat endings.
-  final double repeatEndingLineThickness;
+  final double? repeatEndingLineThickness;
 
   /// The thickness of the end of a slur.
-  final double slurEndpointThickness;
+  final double? slurEndpointThickness;
 
   /// The thickness of the mid-point of a slur (i.e. its thickest point).
-  final double slurMidpointThickness;
+  final double? slurMidpointThickness;
 
   /// The thickness of each staff line
-  final double staffLineThickness;
+  final double? staffLineThickness;
 
   /// The thickness of a stem
-  final double stemThickness;
+  final double? stemThickness;
 
   /// The thickness of the vertical line of a sub-bracket grouping staves
   /// belonging to the same instrument together
-  final double subBracketThickness;
+  final double? subBracketThickness;
 
   /// The thickness of a box drawn around text instructions (e.g. rehearsal marks).
-  final double textEnclosureThickness;
+  final double? textEnclosureThickness;
 
   /// The thickness of a thick barline, e.g. in a final barline or a repeat barline.
-  final double thickBarlineThickness;
+  final double? thickBarlineThickness;
 
   /// The thickness of a thin barline, e.g. a normal barline, or each of the
   /// lines of a double barline.
-  final double thinBarlineThickness;
+  final double? thinBarlineThickness;
 
   /// The thickness of the end of a tie.
-  final double tieEndpointThickness;
+  final double? tieEndpointThickness;
 
   /// The thickness of the mid-point of a tie.
-  final double tieMidpointThickness;
+  final double? tieMidpointThickness;
 
   /// The thickness of the brackets drawn either side of tuplet numbers.
-  final double tupletBracketThickness;
+  final double? tupletBracketThickness;
 
   /// The thickness of the horizontal line drawn between two vertical lines,
   /// known as the H-bar, in a multi-bar rest.
-  final double hBarThickness;
+  final double? hBarThickness;
 
   /// Creates an instance of [EngravingDefaults] with all necessary fields.
   ///
@@ -119,36 +119,36 @@ class EngravingDefaults {
   /// );
   /// ```
   EngravingDefaults({
-    required this.textFontFamily,
-    required this.arrowShaftThickness,
-    required this.barlineSeparation,
-    required this.beamSpacing,
-    required this.beamThickness,
-    required this.bracketThickness,
-    required this.dashedBarlineDashLength,
-    required this.dashedBarlineGapLength,
-    required this.dashedBarlineThickness,
-    required this.hairpinThickness,
-    required this.ledgerLineExtension,
-    required this.ledgerLineThickness,
-    required this.lyricLineThickness,
-    required this.octaveLineThickness,
-    required this.pedalLineThickness,
-    required this.repeatBarlineDotSeparation,
-    required this.repeatEndingLineThickness,
-    required this.slurEndpointThickness,
-    required this.slurMidpointThickness,
-    required this.staffLineThickness,
-    required this.stemThickness,
-    required this.subBracketThickness,
-    required this.textEnclosureThickness,
-    required this.thickBarlineThickness,
-    required this.thinBarlineThickness,
-    required this.tieEndpointThickness,
-    required this.tieMidpointThickness,
-    required this.tupletBracketThickness,
-    required this.thinThickBarlineSeparation,
-    required this.hBarThickness,
+    this.textFontFamily = const [],
+    this.arrowShaftThickness,
+    this.barlineSeparation,
+    this.beamSpacing,
+    this.beamThickness,
+    this.bracketThickness,
+    this.dashedBarlineDashLength,
+    this.dashedBarlineGapLength,
+    this.dashedBarlineThickness,
+    this.hairpinThickness,
+    this.ledgerLineExtension,
+    this.ledgerLineThickness,
+    this.lyricLineThickness,
+    this.octaveLineThickness,
+    this.pedalLineThickness,
+    this.repeatBarlineDotSeparation,
+    this.repeatEndingLineThickness,
+    this.slurEndpointThickness,
+    this.slurMidpointThickness,
+    this.staffLineThickness,
+    this.stemThickness,
+    this.subBracketThickness,
+    this.textEnclosureThickness,
+    this.thickBarlineThickness,
+    this.thinBarlineThickness,
+    this.tieEndpointThickness,
+    this.tieMidpointThickness,
+    this.tupletBracketThickness,
+    this.thinThickBarlineSeparation,
+    this.hBarThickness,
   });
 
   /// Creates an [EngravingDefaults] object from a JSON map.
@@ -279,32 +279,164 @@ class GlyphAnchor {
   }
 }
 
-/// Represents a glyph that contains a list of anchor points.
-///
-/// Each glyph is associated with a name and multiple anchors that define
-/// its positioning or interaction within the font.
+/// Represents the finite set of anchor fields
+enum AnchorField {
+  /// The exact position at which the bottom right-hand (south-east) corner of an
+  /// angled upward-pointing stem connecting the right-hand side of a notehead to
+  /// a vertical stem to its left should start, relative to the glyph origin,
+  /// expressed as Cartesian coordinates in staff spaces.
+  splitStemUpSE,
+
+  /// The exact position at which the bottom left-hand (south-west) corner of an
+  /// angled upward-pointing stem connecting the left-hand side of a notehead to
+  /// a vertical stem to its right should start, relative to the glyph origin,
+  /// expressed as Cartesian coordinates in staff spaces.
+  splitStemUpSW,
+
+  /// The exact position at which the top right-hand (north-east) corner of an
+  /// angled downward-pointing stem connecting the right-hand side of a notehead
+  /// to a vertical stem to its left should start, relative to the glyph origin,
+  /// expressed as Cartesian coordinates in staff spaces.
+  splitStemDownNE,
+
+  /// The exact position at which the top left-hand (north-west) corner of an angled
+  /// downward-pointing stem connecting the left-hand side of a notehead to a
+  /// vertical stem to its right should start, relative to the glyph origin,
+  /// expressed as Cartesian coordinates in staff spaces.
+  splitStemDownNW,
+
+  /// The exact position at which the bottom right-hand (south-east) corner of an
+  /// upward-pointing stem rectangle should start, relative to the glyph origin,
+  /// expressed as Cartesian coordinates in staff spaces.
+  stemUpSE,
+
+  /// The exact position at which the top left-hand (north-west) corner of a
+  /// downward-pointing stem rectangle should start, relative to the glyph origin,
+  /// expressed as Cartesian coordinates in staff spaces.
+  stemDownNW,
+
+  /// The amount by which an up-stem should be lengthened from its nominal unmodified
+  /// length in order to ensure a good connection with a flag, in spaces.1
+  stemUpNW,
+
+  /// The amount by which a down-stem should be lengthened from its nominal unmodified
+  /// length in order to ensure a good connection with a flag, in spaces.
+  stemDownSW,
+
+  /// The width in staff spaces of a given glyph that should be used for e.g.
+  /// positioning leger lines correctly.*
+  ///
+  /// **Certain fonts, for example those that mimic music calligraphy, may include
+  /// glyphs that are asymmetric by design, and where a simple calculation of the
+  /// glyph’s bounding box will not provide the correct result for registering
+  /// that glyph with other primitives. For example, a whole rest may be slightly
+  /// oblique if mimicking a chisel nib pen, and for precise registration it may
+  /// be necessary to specify its width independent of the glyph’s actual bounding box.*
+  nominalWidth,
+
+  /// The position in staff spaces that should be used to position numerals relative
+  /// to clefs with ligated numbers where those numbers hang from the bottom of
+  /// the clef, corresponding horizontally to the center of the numeral’s bounding box.
+  numeralTop,
+
+  /// The position in staff spaces that should be used to position numerals
+  /// relative to clefs with ligatured numbers where those numbers sit on the
+  /// baseline or at the north-east corner of the G clef, corresponding horizontally
+  /// to the center of the numeral’s bounding box.
+  numeralBottom,
+
+  /// The Cartesian coordinates in staff spaces of the bottom left corner of a
+  /// nominal rectangle that intersects the top right corner of the glyph’s
+  /// bounding box. This rectangle, together with those in the other four corners
+  /// of the glyph’s bounding box, can be cut out to produce a more detailed
+  /// bounding box (of abutting rectangles), useful for kerning or interlocking
+  /// symbols such as accidentals.
+  cutOutNE,
+
+  /// The Cartesian coordinates in staff spaces of the top left corner of a nominal
+  /// rectangle that intersects the bottom right corner of the glyph’s bounding box.
+  cutOutSE,
+
+  /// The Cartesian coordinates in staff spaces of the top right corner of a
+  /// nominal rectangle that intersects the bottom left corner of the glyph’s bounding box.
+  cutOutSW,
+
+  /// The Cartesian coordinates in staff spaces of the bottom right corner of a
+  /// nominal rectangle that intersects the top left corner of the glyph’s bounding box.
+  cutOutNW,
+
+  /// The Cartesian coordinates in staff spaces of the position at which the
+  /// glyph graceNoteSlashStemUp should be positioned relative to the stem-up
+  /// flag of an unbeamed grace note; alternatively, the bottom left corner of
+  /// a diagonal line drawn instead of using the above glyph.
+  graceNoteSlashSW,
+
+  /// The Cartesian coordinates in staff spaces of the top right corner of a
+  /// diagonal line drawn instead of using the glyph graceNoteSlashStemUp for
+  /// a stem-up flag of an unbeamed grace note.
+  graceNoteSlashNE,
+
+  /// The Cartesian coordinates in staff spaces of the position at which the glyph
+  /// graceNoteSlashStemDown should be positioned relative to the stem-down flag
+  /// of an unbeamed grace note; alternatively, the top left corner of a
+  /// diagonal line drawn instead of using the above glyph.
+  graceNoteSlashNW,
+
+  /// The Cartesian coordinates in staff spaces of the bottom right corner of a
+  /// diagonal line drawn instead of using the glyph graceNoteSlashStemDown
+  /// for a stem-down flag of an unbeamed grace note.
+  graceNoteSlashSE,
+
+  /// The Cartesian coordinates in staff spaces of the horizontal position at
+  /// which a glyph repeats, i.e. the position at which the same glyph or another
+  /// of the same group should be positioned to ensure correct tessellation.
+  /// This is used for e.g. multi-segment lines and the component glyphs that make up trills and mordents.
+  repeatOffset,
+
+  /// The Cartesian coordinates in staff spaces of the left-hand edge of a notehead
+  /// with a non-zero left-hand side bearing (e.g. a double whole, or breve,
+  /// notehead with two vertical lines at each side), to assist in the correct
+  /// horizontal alignment of these noteheads with other noteheads with zero-width left-side bearings.
+  noteheadOrigin,
+
+  /// The Cartesian coordinates in staff spaces of the optical center of the glyph,
+  /// to assist in the correct horizontal alignment of the glyph relative to a
+  /// notehead or stem. Currently recommended for use with glyphs in the Dynamics range.
+  opticalCenter;
+
+  // A mapping of string keys to enum values
+  static const Map<String, AnchorField> map = {
+    'splitStemUpSE': AnchorField.splitStemUpSE,
+    'splitStemUpSW': AnchorField.splitStemUpSW,
+    'splitStemDownNE': AnchorField.splitStemDownNE,
+    'splitStemDownNW': AnchorField.splitStemDownNW,
+    'stemUpSE': AnchorField.stemUpSE,
+    'stemDownNW': AnchorField.stemDownNW,
+    'stemUpNW': AnchorField.stemUpNW,
+    'stemDownSW': AnchorField.stemDownSW,
+    'nominalWidth': AnchorField.nominalWidth,
+    'numeralTop': AnchorField.numeralTop,
+    'numeralBottom': AnchorField.numeralBottom,
+    'cutOutNE': AnchorField.cutOutNE,
+    'cutOutSE': AnchorField.cutOutSE,
+    'cutOutSW': AnchorField.cutOutSW,
+    'cutOutNW': AnchorField.cutOutNW,
+    'graceNoteSlashSW': AnchorField.graceNoteSlashSW,
+    'graceNoteSlashNE': AnchorField.graceNoteSlashNE,
+    'graceNoteSlashNW': AnchorField.graceNoteSlashNW,
+    'graceNoteSlashSE': AnchorField.graceNoteSlashSE,
+    'repeatOffset': AnchorField.repeatOffset,
+    'noteheadOrigin': AnchorField.noteheadOrigin,
+    'opticalCenter': AnchorField.opticalCenter,
+  };
+}
+
+// Class to represent glyphs with anchors using enum for anchor fields
 class GlyphWithAnchor {
-  /// The name of the glyph (e.g., "accidentalSharp", "noteheadQuarter").
   final String glyphName;
+  final Map<AnchorField, List<double>> anchors;
 
-  /// A list of anchor points associated with the glyph.
-  ///
-  /// Each anchor provides details about specific points on the glyph,
-  /// such as where certain elements should attach.
-  final List<GlyphAnchor> anchors;
-
-  /// Creates an instance of [GlyphWithAnchor] with the specified glyph name and anchors.
-  ///
-  /// Example:
-  /// ```dart
-  /// var glyphWithAnchor = GlyphWithAnchor(
-  ///   glyphName: 'noteheadQuarter',
-  ///   anchors: [
-  ///     GlyphAnchor(name: 'top', coordinates: [0.0, 1.0]),
-  ///     GlyphAnchor(name: 'bottom', coordinates: [0.0, -1.0]),
-  ///   ],
-  /// );
-  /// ```
+  /// Creates an instance of [GlyphWithAnchor] with a glyph name and anchors.
   GlyphWithAnchor({
     required this.glyphName,
     required this.anchors,
@@ -312,26 +444,29 @@ class GlyphWithAnchor {
 
   /// Creates a [GlyphWithAnchor] from a JSON map.
   ///
-  /// The [json] parameter must contain a `glyphName` and an `anchors` list.
-  ///
-  /// Example:
-  /// ```dart
-  /// var json = {
-  ///   'glyphName': 'noteheadQuarter',
-  ///   'anchors': [
-  ///     {'name': 'top', 'coordinates': [0.0, 1.0]},
-  ///     {'name': 'bottom', 'coordinates': [0.0, -1.0]}
-  ///   ]
-  /// };
-  /// var glyphWithAnchor = GlyphWithAnchor.fromJson(json);
-  /// ```
-  factory GlyphWithAnchor.fromJson(Map<String, dynamic> json) {
+  /// The JSON map should have keys that match the anchor field names and values that are coordinate arrays.
+  factory GlyphWithAnchor.fromJson(
+    String glyphName,
+    Map<String, dynamic> json,
+  ) {
+    final Map<AnchorField, List<double>> anchors = {};
+
+    // Iterate over the JSON map and populate the anchors map with enum keys
+    json.forEach((key, value) {
+      if (AnchorField.map.containsKey(key)) {
+        anchors[AnchorField.map[key]!] = List<double>.from(value);
+      }
+    });
+
     return GlyphWithAnchor(
-      glyphName: json['glyphName'],
-      anchors: (json['anchors'] as List)
-          .map((anchor) => GlyphAnchor.fromJson(anchor))
-          .toList(),
+      glyphName: glyphName,
+      anchors: anchors,
     );
+  }
+
+  /// Helper method to retrieve anchor data by [AnchorField].
+  List<double>? getAnchor(AnchorField field) {
+    return anchors[field];
   }
 }
 
@@ -424,14 +559,14 @@ class FontMetadata {
     // Parse glyphs with anchors
     List<GlyphWithAnchor> glyphsWithAnchors = [];
     if (json['glyphsWithAnchors'] != null) {
-      glyphsWithAnchors = (json['glyphsWithAnchors'] as List)
-          .map((glyph) => GlyphWithAnchor.fromJson(glyph))
-          .toList();
+      json['glyphsWithAnchors'].forEach((key, value) {
+        glyphsWithAnchors.add(GlyphWithAnchor.fromJson(key, value));
+      });
     }
 
     return FontMetadata(
       fontName: json['fontName'],
-      fontVersion: json['fontVersion'],
+      fontVersion: json['fontVersion'].toDouble(),
       engravingDefaults: EngravingDefaults.fromJson(json['engravingDefaults']),
       glyphBBoxes: bBoxes,
       glyphsWithAnchors: glyphsWithAnchors,
