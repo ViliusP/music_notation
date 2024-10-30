5b96b2159882875482d22c912d9beaea6d3817d6
ViliusP <viliuspaliokas@gmail.com>
2024-10-30 12:06:29 +0200

    Add new AlignmentPosition to MeasureWidget

--- lib/src/notation_painter/measure/measure_element.dart
+++ lib/src/notation_painter/measure/measure_element.dart
@@ -3,39 +3,65 @@ import 'package:flutter/widgets.dart';
 import 'package:music_notation/src/notation_painter/models/element_position.dart';
 import 'package:music_notation/src/notation_painter/notation_layout_properties.dart';
 
-/// An abstract base class for a measure widget that represents a musical element
-/// with properties related to its positioning, size, and alignment within a measure.
+/// An abstract base class representing a musical element within a measure,
+/// including properties for positioning, size, and alignment.
 abstract class MeasureWidget extends Widget {
-  /// The position of the musical element within the measure, represented by an `ElementPosition` object.
-  /// This includes information such as the step (A-G) and octave, allowing precise
-  /// definition and comparison of the element's position on the musical staff.
-  /// It enables accurate placement of elements according to the diatonic scale.
+  /// Position of the musical element within the measure, using an [ElementPosition]
+  /// object to define note pitch and octave. This allows accurate placement on the staff.
   ElementPosition get position;
 
   /// The size of the element, defining its width and height within the measure.
   Size get size;
 
+  /// Deprecated!
+  ///
   /// The offset of the element's position within its parent container or measure.
   /// This defines how much the element is shifted vertically from its default position.
   ///
   /// Measurement is defined from the element bounding box top.
   double get verticalAlignmentAxisOffset;
 
+  /// Deprecated!
+  ///
   /// The alignment offset from the left side of the element's rectangle.
   /// It represents the axis point for aligning the element with others,
   /// typically being the middle of most elements but adjustable for visual alignment.
   double get alignmentOffset;
 
-  /// A constant constructor for the [MeasureWidget] to ensure that subclasses can be instantiated
-  /// with constant expressions if all their fields are constant.
+  /// Optional positioning and alignment information for precise element placement.
+  AlignmentPosition? get alignmentPosition;
+
+  /// Constant constructor for the [MeasureWidget] base class.
   const MeasureWidget({super.key});
 }
 
-/// A mixin that provides methods related to element dimensions and positioning on the musical staff.
-/// This mixin can be applied to any class that extends [MeasureWidget] and provides the necessary properties.
-extension MeasureElemenetDimensions on MeasureWidget {
-  /// Calculates the rectangular box below the staff where the element might be positioned.
-  /// This is particularly useful for elements that extend below the musical staff, like ledger lines.
+/// Defines specific alignment offsets for musical elements, used for vertical and
+/// horizontal positioning within their container.
+class AlignmentPosition {
+  /// Vertical offset from the top of the bounding box, aligning the element with
+  /// the staff line when positioned at `Y=0`. If null, alignment is based on [bottom].
+  final double? top;
+
+  /// Vertical offset from the bottom of the bounding box, aligning the element with
+  /// the staff line when positioned at `Y=container.height`. If null, alignment is based on [top].
+  final double? bottom;
+
+  /// Horizontal offset from the left side of the element’s bounding box, aligning the
+  /// element horizontally, typically at the visual or optical center.
+  final double? left;
+
+  const AlignmentPosition({
+    required this.top,
+    required this.bottom,
+    required this.left,
+  });
+}
+
+/// A mixin for calculating bounding boxes of elements positioned above or below
+/// the staff, particularly useful for handling elements like ledger lines.
+extension MeasureElementDimensions on MeasureWidget {
+  /// Calculates a bounding box for elements extending below the staff,
+  /// with consideration for the element's position and vertical alignment offset.
   Rect boxBelowStaff() {
     const offsetPerPosition = NotationLayoutProperties.staveSpace / 2;
 
@@ -54,6 +80,8 @@ extension MeasureElemenetDimensions on MeasureWidget {
     return Rect.fromPoints(Offset(0, 0), Offset(size.width, belowStaffLength));
   }
 
+  /// Calculates a bounding box for elements extending above the staff,
+  /// considering the element's position and vertical alignment offset.
   Rect boxAboveStaff() {
     const offsetPerPosition = NotationLayoutProperties.staveSpace / 2;
 
@@ -63,7 +91,7 @@ extension MeasureElemenetDimensions on MeasureWidget {
     double aboveStaffLength = [
       offsetPerPosition * position.numeric,
       verticalAlignmentAxisOffset,
-      -distanceToStaffTop
+      -distanceToStaffTop,
     ].sum;
 
     aboveStaffLength = [0.0, aboveStaffLength].max;
