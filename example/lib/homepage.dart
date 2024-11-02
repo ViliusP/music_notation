6eac412bf6a6b58855580e40c27fbdabb8a83bf1
ViliusP <viliuspaliokas@gmail.com>
2024-11-02 21:08:27 +0200

    improve debug

--- example/lib/homepage.dart
+++ example/lib/homepage.dart
@@ -1,5 +1,7 @@
+import 'package:collection/collection.dart';
 import 'package:example/music_painter_debug.dart';
 import 'package:flutter/material.dart';
+import 'package:music_notation/music_notation.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -9,13 +11,59 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  double? calcWidth(GlyphBBox? bBoxes) {
+    if (bBoxes == null) {
+      return double.maxFinite;
+    }
+    return (bBoxes.bBoxNE.x * 20 - bBoxes.bBoxSW.x * 20).abs();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: CustomPaint(
-        painter: MusicPainter(),
+      padding: const EdgeInsets.all(64.0),
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          CustomPaint(
+            size: Size(1000, 80),
+            painter: StaffPainter(),
+          ),
+          ...glyphs.mapIndexed(
+            (i, g) => Positioned(
+              left: (i * 200) + 50,
+              child: SizedBox(
+                height: 80,
+                width: calcWidth(bBoxes.elementAt(i)),
+                child: CustomPaint(
+                  painter: SmuflPainter(g, bBoxes.elementAt(i)),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
+List<SmuflGlyph> glyphs = [
+  SmuflGlyph.gClef,
+  SmuflGlyph.noteheadBlack,
+  SmuflGlyph.accidentalFlat,
+];
+
+List<GlyphBBox?> bBoxes = [
+  GlyphBBox(
+    bBoxNE: Coordinates(x: 2.556, y: 4.448),
+    bBoxSW: Coordinates(x: 0.0, y: -2.664),
+  ),
+  GlyphBBox(
+    bBoxNE: Coordinates(x: 1.3, y: 0.528),
+    bBoxSW: Coordinates(x: 0.0, y: -0.532),
+  ),
+  GlyphBBox(
+    bBoxNE: Coordinates(x: 0.812, y: 1.812),
+    bBoxSW: Coordinates(x: 0.0, y: -0.704),
+  )
+];
--- example/lib/music_painter_debug.dart
+++ example/lib/music_painter_debug.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/rendering.dart';
+import 'package:music_notation/music_notation.dart';
 
 const double _staffLineSpacing = 20; // Distance between staff lines
 
-class MusicPainter extends CustomPainter {
-  MusicPainter();
+class StaffPainter extends CustomPainter {
+  StaffPainter();
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -16,45 +17,6 @@ class MusicPainter extends CustomPainter {
       double y = i * _staffLineSpacing;
       canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
     }
-    List<String> glyphs = [
-      '\uE050',
-      '\uE084',
-      '\uE240',
-      '\uE0A4',
-      '\uE4E3',
-      '\uE4E4',
-      '\uE1D5',
-      Accidentals
-      // '\uD158',
-    ];
-    for (int i = 0; i < glyphs.length; i++) {
-      // Set the font size to 4 times the staff spacing
-      final textPainter = _bravuraPainter(glyphs[i]);
-
-      // Position the notehead on the staff
-      textPainter.layout();
-      // For this example, we position it relative to the bottom line of the staff
-      textPainter.paint(
-        canvas,
-        Offset(50 + i * 100, _staffLineSpacing),
-      ); // E4 note on the bottom line
-    }
-
-    for (int i = 0; i < 3; i++) {
-      // if ([0, 2].contains(i)) continue;
-      // Set the font size to 4 times the staff spacing
-      // double enlargeBy = 1;
-      // final textPainter = _bravuraPainter('\uE0A4', enlargeBy);
-      final textPainter = _bravuraPainter('\uE0A4');
-
-      // Position the notehead on the staff
-      textPainter.layout();
-      // For this example, we position it relative to the bottom line of the staff
-      textPainter.paint(
-        canvas,
-        Offset(80 + 6 * 100, (_staffLineSpacing * i)),
-      ); // E4 note on the bottom line
-    }
   }
 
   @override
@@ -63,21 +25,127 @@ class MusicPainter extends CustomPainter {
   }
 }
 
-TextPainter _bravuraPainter(String glyph, [double enlargeBy = 1.0]) {
-  return TextPainter(
-    text: TextSpan(
-      text: glyph, // Example notehead (G clef) from SMuFL font
-      style: TextStyle(
-          fontFamily: 'Leland',
-          fontSize: _staffLineSpacing *
-              4 *
-              enlargeBy, // Corrected font size for notehead
-          color: Color.fromRGBO(0, 0, 0, 1),
-          height: 1,
-          // textBaseline: TextBaseline.ideographic,
-          // decoration: TextDecoration.underline,
-          backgroundColor: Color.fromRGBO(255, 100, 2, 0.2)),
-    ),
-    textDirection: TextDirection.ltr,
-  );
+class SmuflPainter extends CustomPainter {
+  final SmuflGlyph glyph;
+  final GlyphBBox? glyphBBox;
+
+  SmuflPainter(this.glyph, [this.glyphBBox]);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final textPainter = _leLandPainter(glyph.codepoint);
+
+    // Position the notehead on the staff
+    textPainter.layout();
+    // For this example, we position it relative to the bottom line of the staff
+    textPainter.paint(
+      canvas,
+      Offset(0, 0),
+    );
+    if (glyphBBox != null) {
+      final paint = Paint()
+        ..color = Color.fromRGBO(0, 0, 0, 0.1)
+        ..strokeWidth = 1.0
+        ..style = PaintingStyle.stroke;
+
+      canvas.drawRect(
+        Rect.fromPoints(
+          Offset(0, 0),
+          Offset(size.width, size.height),
+        ),
+        paint,
+      );
+    }
+    if (glyphBBox != null) {
+      final paint = Paint()
+        ..color = Color.fromRGBO(0, 0, 0, 0.5)
+        ..strokeWidth = 1.0
+        ..style = PaintingStyle.stroke;
+
+      Offset o1 =
+          fromCoordinates(c1(glyphBBox!)).scale(20, 20).translate(0, 40);
+      Offset o2 =
+          fromCoordinates(c2(glyphBBox!)).scale(20, 20).translate(0, 40);
+
+      canvas.drawRect(
+        Rect.fromPoints(o1, o2),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+
+  Offset fromCoordinates(Coordinates c) {
+    return Offset(c.x, c.y);
+  }
+
+  Coordinates c1(GlyphBBox bBox) {
+    Coordinates ne = bBox.bBoxNE;
+    Coordinates sw = bBox.bBoxSW;
+
+    // Calculate NW point in the original Cartesian coordinate system
+    Coordinates nw = Coordinates(x: sw.x, y: -ne.y);
+
+    return nw;
+  }
+
+  Coordinates c2(GlyphBBox bBox) {
+    Coordinates ne = bBox.bBoxNE;
+    Coordinates sw = bBox.bBoxSW;
+
+    // Calculate SE point in the original Cartesian coordinate system
+    Coordinates se = Coordinates(x: ne.x, y: -sw.y);
+
+    return se;
+  }
+
+  // Coordinates c1(GlyphBBox bBox) {
+  //   Coordinates ne = bBox.bBoxNE;
+  //   Coordinates sw = bBox.bBoxSW;
+
+  //   // Calculate NW point in the original Cartesian coordinate system
+  //   Coordinates nw = Coordinates(x: sw.x, y: ne.y);
+  //   // Shift coordinates by minX and minY (bottom-left corner) to make all values non-negative
+  //   double minX = sw.x;
+  //   double minY = sw.y;
+
+  //   return Coordinates(x: nw.x - minX, y: nw.y - minY);
+  // }
+
+  // Coordinates c2(GlyphBBox bBox) {
+  //   Coordinates ne = bBox.bBoxNE;
+  //   Coordinates sw = bBox.bBoxSW;
+
+  //   // Calculate SE point in the original Cartesian coordinate system
+  //   Coordinates se = Coordinates(x: ne.x, y: sw.y);
+
+  //   // Shift coordinates by minX and minY (bottom-left corner) to make all values non-negative
+  //   double minX = sw.x;
+  //   double minY = sw.y;
+
+  //   return Coordinates(x: se.x - minX, y: se.y - minY);
+  // }
+
+  TextPainter _leLandPainter(String glyph, [double enlargeBy = 1.0]) {
+    return TextPainter(
+      text: TextSpan(
+        text: glyph, // Example notehead (G clef) from SMuFL font
+        style: TextStyle(
+            fontFamily: 'Leland',
+            fontSize: _staffLineSpacing *
+                4 *
+                enlargeBy, // Corrected font size for notehead
+            color: Color.fromRGBO(0, 0, 0, 1),
+            height: 1,
+            // textBaseline: TextBaseline.ideographic,
+            // decoration: TextDecoration.underline,
+            backgroundColor: Color.fromRGBO(255, 100, 2, 0.05)),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+  }
 }
