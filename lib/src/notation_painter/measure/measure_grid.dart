20f85712d55548e79b4bbe4a33c95f83b6d53ef1
ViliusP <viliuspaliokas@gmail.com>
2024-11-25 13:20:59 +0200

    New data structure for music sheet

---
+++ lib/src/notation_painter/measure/measure_grid.dart
@@ -0,0 +1,403 @@
+import 'dart:collection';
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+import 'package:music_notation/src/notation_painter/measure/barline_painting.dart';
+import 'package:music_notation/src/notation_painter/measure/measure_element.dart';
+import 'package:music_notation/src/notation_painter/models/element_position.dart';
+import 'package:music_notation/src/notation_painter/spacing/timeline.dart';
+import 'package:music_notation/src/notation_painter/utilities/number_extensions.dart';
+
+class MusicSheetGrid {
+  /// Outer list represents measure location horizontally (measure number), inner list represents stave position.
+  final List<List<MeasureGrid>> _values;
+  List<List<MeasureGrid>> get values => _values;
+
+  final int staves;
+
+  MusicSheetGrid(this.staves) : _values = [];
+
+  void add(List<MeasureGrid> column) {
+    if (staves != column.length) {
+      throw ArgumentError(
+        "Provided measures has bigger size than grid has staves",
+      );
+    }
+
+    List<MeasureGrid> syncedWidth = _syncMeasuresWidth(column);
+    List<MeasureGrid> syncedHeight = [];
+    for (var (i, measure) in syncedWidth.indexed) {
+      syncedHeight.add(_syncMeasureHeight(measure, i));
+    }
+
+    _values.add(syncedHeight);
+  }
+
+  List<MeasureGrid> _syncMeasuresWidth(List<MeasureGrid> measures) {
+    return List.from(measures, growable: false);
+  }
+
+  MeasureGrid _syncMeasureHeight(MeasureGrid measure, int staff) {
+    var modifiedMeasure = measure.copyWith();
+    if (_values.isEmpty) {
+      return modifiedMeasure;
+    }
+
+    var lastStaffMeasure = _values.last[staff];
+    int currentHeightAboveStave = lastStaffMeasure.heightAboveStave;
+    int currentHeightBelowStave = lastStaffMeasure.heightBelowStave;
+
+    if (currentHeightAboveStave > measure.heightAboveStave) {
+      modifiedMeasure.setHeightAbove(currentHeightAboveStave);
+    }
+    if (currentHeightAboveStave < measure.heightAboveStave) {
+      _setHeightAbove(measure.heightAboveStave, staff);
+    }
+    if (currentHeightBelowStave > measure.heightBelowStave) {
+      modifiedMeasure.setHeightBelow(currentHeightBelowStave);
+    }
+    if (currentHeightBelowStave < measure.heightBelowStave) {
+      _setHeightBelow(measure.heightBelowStave, staff);
+    }
+
+    return modifiedMeasure;
+  }
+
+  void _setHeightAbove(int height, int staff) {
+    for (var measureCol in _values) {
+      measureCol[staff].setHeightAbove(height);
+    }
+  }
+
+  void _setHeightBelow(int height, int staff) {
+    for (var measureCol in _values) {
+      measureCol[staff].setHeightBelow(height);
+    }
+  }
+
+  @override
+  String toString() {
+    int length = _values.elementAtOrNull(0)?.length ?? 0;
+
+    List<List<String>> repr = List.generate(length, (_) => []);
+    for (var (i, col) in _values.indexed) {
+      for (var (j, _) in col.indexed) {
+        repr[j].add("($j,$i)");
+      }
+    }
+
+    String representation = "";
+    for (var row in repr) {
+      int colWidth = row.elementAtOrNull(0)?.length ?? 0;
+      representation += "\n| ";
+
+      for (var col in row) {
+        colWidth = max(colWidth, col.length);
+      }
+      for (var col in row) {
+        representation += "$col | ".padLeft(colWidth);
+      }
+    }
+
+    return representation;
+  }
+}
+
+/// Visual representation
+///
+/// ```text
+///   0   1   2   3   column
+/// ==-===-===-===-== F5  (38)
+/// | - | - | - | - | E5  (37)
+/// ==-===-===-===-== D5
+/// | - | - | - | - | C5
+/// ==-===-===-===-== B4
+/// | - | - | - | - | A4
+/// ==-===-===-===-== G4
+/// | - | - | - | - | F4  (31)
+/// ==-===-===-===-== E3  (30)
+///                   Row (index)
+/// ```
+///
+/// `=` - represents staff line and cell's top/bottom boundary.\
+/// `-` - represents position.\
+/// `|` - cell vertical and cell's left/right boundary
+class MeasureGrid {
+  final List<MeasureWidget> _children;
+  final BarlineSettings _barlineSettings;
+  final BeatTimelineV2 _beatTimelineV2;
+  final MeasureTimeline _timeline;
+  final int minHeightAbove;
+  final int minHeightBelow;
+
+  final SplayTreeMap<ColumnIndex, MeasureGridColumn> _columns;
+
+  SplayTreeMap<ColumnIndex, MeasureGridColumn> get columns => _columns;
+
+  const MeasureGrid._({
+    required List<MeasureWidget> children,
+    required BeatTimelineV2 beatTimelineV2,
+    required BarlineSettings barlineSettings,
+    required MeasureTimeline timeline,
+    required SplayTreeMap<ColumnIndex, MeasureGridColumn> columns,
+    this.minHeightAbove = 0,
+    this.minHeightBelow = 0,
+  })  : _beatTimelineV2 = beatTimelineV2,
+        _barlineSettings = barlineSettings,
+        _timeline = timeline,
+        _columns = columns,
+        _children = children;
+
+  factory MeasureGrid.fromMeasureWidgets({
+    required List<MeasureWidget> children,
+    required BarlineSettings barlineSettings,
+    int minHeightAbove = 0,
+    int minHeightBelow = 0,
+  }) {
+    MeasureTimeline timeline = MeasureTimeline.fromMeasureElements(children);
+    BeatTimelineV2 beatTimeline = BeatTimelineV2.fromTimeline(timeline);
+
+    SplayTreeMap<ColumnIndex, MeasureGridColumn> columns = SplayTreeMap.of({});
+    int heightBelowStaff = minHeightBelow;
+    int heightAboveStaff = minHeightAbove;
+    int count = 0;
+    for (var entry in timeline.values.entries) {
+      List<TimelineValue> beatCol = entry.value.sorted(
+        (a, b) => a.voice.compareTo(b.voice),
+      );
+
+      MeasureGridColumn col = MeasureGridColumn.fromHeights(
+        heightAboveStave: minHeightAbove,
+        heightBelowStave: minHeightBelow,
+      );
+
+      for (var (i, value) in beatCol.indexed) {
+        var position = children[value.index].position;
+
+        if (value.duration == 0) {
+          MeasureGridColumn col = MeasureGridColumn.fromHeights(
+            heightAboveStave: minHeightAbove,
+            heightBelowStave: minHeightBelow,
+          );
+          col.set(position, children[value.index]);
+
+          columns[ColumnIndex(count, false)] = col;
+          count++;
+        } else {
+          col.set(position, children[value.index]);
+          if (i == beatCol.length - 1) {
+            columns[ColumnIndex(count, true)] = col;
+            count++;
+          }
+        }
+
+        var elementHeightBelowStaff = position.distanceFromBottom
+            .clamp(NumberConstants.minFiniteInt, 0)
+            .abs();
+
+        heightBelowStaff = max(heightBelowStaff, elementHeightBelowStaff);
+
+        var elementHeightAboveStaff = position.distanceFromTop.clamp(
+          0,
+          NumberConstants.maxFiniteInt,
+        );
+
+        heightAboveStaff = max(heightAboveStaff, elementHeightAboveStaff);
+      }
+    }
+    for (var e in columns.entries) {
+      e.value.setHeightBelow(heightBelowStaff);
+      e.value.setHeightAbove(heightAboveStaff);
+    }
+
+    return MeasureGrid._(
+      barlineSettings: barlineSettings,
+      timeline: timeline,
+      beatTimelineV2: beatTimeline,
+      children: children,
+      minHeightAbove: minHeightBelow,
+      minHeightBelow: minHeightBelow,
+      columns: columns,
+    );
+  }
+
+  int get heightAboveStave {
+    var highestPosition = _columns.values.first._values.keys.firstOrNull;
+    highestPosition ??= ElementPosition.staffTop;
+
+    int distance = ElementPosition.staffTop.distance(highestPosition);
+
+    return distance.clamp(0, NumberConstants.maxFiniteInt);
+  }
+
+  int get heightBelowStave {
+    var lowestPosition = _columns.values.first._values.keys.firstOrNull;
+    lowestPosition ??= ElementPosition.staffBottom;
+
+    int distance = ElementPosition.staffBottom.distance(lowestPosition);
+
+    return distance.clamp(0, NumberConstants.maxFiniteInt);
+  }
+
+  static List<List<MeasureGridColumn>> toMeasureColumns(
+    List<MeasureGrid> measures,
+  ) {
+    final int maxLength = measures
+        .map((list) => list.columns.entries.length)
+        .reduce((a, b) => a > b ? a : b);
+    return List.generate(
+      maxLength,
+      (col) => measures.map((row) => row.columns.values.toList()[col]).toList(),
+    );
+  }
+
+  @override
+  String toString() {
+    int rows = _columns.entries.elementAtOrNull(0)?.value._values.length ?? 0;
+
+    List<List<String>> representationGrid = List.generate(rows, (_) => []);
+
+    for (var (i, entry) in _columns.entries.indexed) {
+      print(entry.value._values.length);
+      for (var (j, colValue) in entry.value._values.entries.indexed) {
+        if (i == 0) {
+          representationGrid[j].add("${colValue.key.numeric}");
+        }
+        if (colValue.value != null) {
+          representationGrid[j].add(
+            "${colValue.key.step}${colValue.key.octave}",
+          );
+        }
+        if (colValue.value == null) {
+          representationGrid[j].add("  ");
+        }
+      }
+    }
+    String repr = "\n";
+    for (var col in representationGrid) {
+      for (var row in col) {
+        repr += "| ${row} ";
+      }
+      repr += "|\n";
+    }
+
+    return repr;
+  }
+
+  void setHeightBelow(int height) {
+    for (var e in _columns.entries) {
+      e.value.setHeightBelow(height);
+    }
+  }
+
+  void setHeightAbove(int height) {
+    for (var e in _columns.entries) {
+      e.value.setHeightAbove(height);
+    }
+  }
+
+  MeasureGrid copyWith() {
+    return MeasureGrid.fromMeasureWidgets(
+      children: _children,
+      barlineSettings: _barlineSettings,
+      minHeightAbove: minHeightAbove,
+      minHeightBelow: minHeightBelow,
+    );
+  }
+}
+
+class MeasureGridColumn {
+  final SplayTreeMap<ElementPosition, MeasureWidget?> _values;
+  SplayTreeMap<ElementPosition, MeasureWidget?> get values => _values;
+
+  MeasureGridColumn()
+      : _values = SplayTreeMap.of({
+          ElementPosition.staffBottom.transpose(0): null,
+          ElementPosition.staffBottom.transpose(1): null,
+          ElementPosition.staffBottom.transpose(2): null,
+          ElementPosition.staffBottom.transpose(3): null,
+          ElementPosition.staffBottom.transpose(4): null,
+          ElementPosition.staffBottom.transpose(5): null,
+          ElementPosition.staffBottom.transpose(6): null,
+          ElementPosition.staffBottom.transpose(7): null,
+          ElementPosition.staffBottom.transpose(8): null, // Staff top
+        }, (a, b) => b.compareTo(a));
+
+  factory MeasureGridColumn.fromHeights({
+    int heightAboveStave = 0,
+    int heightBelowStave = 0,
+  }) {
+    return MeasureGridColumn()
+      ..setHeightAbove(heightAboveStave)
+      ..setHeightBelow(heightBelowStave);
+  }
+
+  void setHeightAbove(int height) {
+    int start = ElementPosition.staffTop.numeric;
+
+    for (int i = start + 1; i < start + height + 1; i++) {
+      if (!_values.containsKey(ElementPosition.fromInt(i))) {
+        _values[ElementPosition.fromInt(i)] = null;
+      }
+    }
+  }
+
+  void setHeightBelow(int height) {
+    int start = ElementPosition.staffBottom.numeric;
+
+    for (int i = start - height; i < start; i++) {
+      if (!_values.containsKey(ElementPosition.fromInt(i))) {
+        _values[ElementPosition.fromInt(i)] = null;
+      }
+    }
+  }
+
+  void set(ElementPosition position, MeasureWidget? widget) {
+    _values[position] = widget;
+  }
+
+  @override
+  String toString() {
+    String value = "\n";
+
+    for (var e in _values.entries) {
+      value += "${e.key.numeric} ${e.key.step}${e.key.octave}: ${e.value}";
+      value += "\n";
+    }
+    return value;
+  }
+}
+
+class ColumnIndex implements Comparable<ColumnIndex> {
+  final int value;
+  final bool isRhytmic;
+
+  ColumnIndex(
+    this.value, [
+    this.isRhytmic = true,
+  ]);
+
+  @override
+  int compareTo(ColumnIndex other) {
+    if (value != other.value) {
+      return value.compareTo(other.value); // Compare by value first
+    }
+    return isRhytmic == other.isRhytmic
+        ? 0
+        : (isRhytmic ? 1 : -1); // `isRhytmic` as secondary comparison
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ColumnIndex &&
+          runtimeType == other.runtimeType &&
+          value == other.value &&
+          isRhytmic == other.isRhytmic;
+
+  @override
+  int get hashCode => Object.hash(value, isRhytmic);
+
+  @override
+  String toString() => "$value${!isRhytmic ? "*" : ""}";
+}
--- lib/src/notation_painter/music_sheet/music_sheet.dart
+++ lib/src/notation_painter/music_sheet/music_sheet.dart
@@ -1,12 +1,17 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/widgets.dart';
+import 'package:music_notation/src/models/data_types/step.dart';
 import 'package:music_notation/src/models/elements/score/score.dart';
 import 'package:music_notation/src/notation_painter/measure/barline_painting.dart';
 import 'package:music_notation/src/notation_painter/measure/measure_element.dart';
-import 'package:music_notation/src/notation_painter/measure/measure_layout.dart';
+import 'package:music_notation/src/notation_painter/measure/measure_grid.dart';
 import 'package:music_notation/src/notation_painter/measure/notation_widgetization.dart';
+import 'package:music_notation/src/notation_painter/measure/staff_lines.dart';
+import 'package:music_notation/src/notation_painter/models/element_position.dart';
 import 'package:music_notation/src/notation_painter/models/notation_context.dart';
 import 'package:music_notation/src/notation_painter/music_grid.dart';
-import 'package:music_notation/src/notation_painter/music_sheet/grid.dart';
 import 'package:music_notation/src/notation_painter/properties/layout_properties.dart';
 import 'package:music_notation/src/notation_painter/properties/notation_properties.dart';
 import 'package:music_notation/src/smufl/font_metadata.dart';
@@ -17,7 +22,7 @@ typedef _MeasureData = ({
 });
 
 class MusicSheet extends StatelessWidget {
-  final List<List<MeasureLayout>> parts;
+  final MusicSheetGrid grid;
 
   final FontMetadata font;
 
@@ -25,7 +30,7 @@ class MusicSheet extends StatelessWidget {
 
   const MusicSheet._({
     super.key,
-    required this.parts,
+    required this.grid,
     required this.font,
     required this.layoutProperties,
   });
@@ -38,14 +43,16 @@ class MusicSheet extends StatelessWidget {
         const NotationLayoutProperties.standard(),
   }) {
     var grid = RawMeasureGrid.fromScoreParts(score.parts);
-    List<List<MeasureLayout>> parts = [];
+    MusicSheetGrid musicSheetGrid = MusicSheetGrid(grid.data.rowCount);
+    var contexts = List.generate(
+      grid.data.rowCount,
+      (_) => NotationContext.empty(),
+    );
+    for (int j = 0; j < grid.data.columnCount; j++) {
+      List<_MeasureData> col = [];
+      for (var i = 0; i < grid.data.rowCount; i++) {
+        int? staff = grid.staffForRow(i);
 
-    for (int i = 0; i < grid.data.rowCount; i++) {
-      NotationContext lastNotationContext = NotationContext.empty();
-      int? staff = grid.staffForRow(i);
-      List<_MeasureData> row = [];
-
-      for (var j = 0; j < grid.data.columnCount; j++) {
         var barlineSettings = BarlineSettings.fromGridData(
           gridX: j,
           gridY: i,
@@ -56,34 +63,34 @@ class MusicSheet extends StatelessWidget {
         );
 
         var children = NotationWidgetization.widgetsFromMeasure(
-          context: lastNotationContext,
+          context: contexts[i],
           staff: staff,
           measure: grid.data.getValue(i, j),
           font: font,
         );
 
-        row.add((children: children, barlineSettings: barlineSettings));
+        col.add((children: children, barlineSettings: barlineSettings));
 
-        lastNotationContext = NotationWidgetization.contextFromWidgets(
+        contexts[i] = NotationWidgetization.contextFromWidgets(
           children,
-          lastNotationContext,
+          contexts[i],
         );
       }
-      var measures = <MeasureLayout>[];
+      var colMeasures = <MeasureGrid>[];
 
-      for (var data in row) {
-        var measure = MeasureLayout(
+      for (var data in col) {
+        var measure = MeasureGrid.fromMeasureWidgets(
           barlineSettings: data.barlineSettings,
           children: data.children,
         );
-        measures.add(measure);
+        colMeasures.add(measure);
       }
-      parts.add(measures);
+      musicSheetGrid.add(colMeasures);
     }
 
     return MusicSheet._(
       key: key,
-      parts: parts,
+      grid: musicSheetGrid,
       font: font,
       layoutProperties: layoutProperties,
     );
@@ -91,15 +98,127 @@ class MusicSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NotationProperties(
-      font: font,
-      layout: layoutProperties,
-      child: Column(
-          children: parts
-              .map(
-                (p) => AlignedRow(children: p),
-              )
-              .toList()),
+    return Wrap(
+      direction: Axis.horizontal,
+      alignment: WrapAlignment.start,
+      spacing: 0,
+      runAlignment: WrapAlignment.start,
+      runSpacing: 0,
+      crossAxisAlignment: WrapCrossAlignment.start,
+      textDirection: TextDirection.ltr,
+      verticalDirection: VerticalDirection.up,
+      clipBehavior: Clip.none,
+      children: grid.values
+          .map((col) => _SheetMeasuresColumn(
+                values: col,
+              ))
+          .toList(),
+    );
+  }
+}
+
+class _SheetMeasuresColumn extends StatelessWidget {
+  final List<MeasureGrid> values;
+
+  const _SheetMeasuresColumn({super.key, required this.values});
+
+  @override
+  Widget build(BuildContext context) {
+    List<List<MeasureGridColumn>> columnColumns = MeasureGrid.toMeasureColumns(
+      values,
+    );
+
+    NotationLayoutProperties layoutProperties =
+        NotationProperties.of(context)?.layout ??
+            NotationLayoutProperties.standard();
+
+    return Row(
+      children: columnColumns.mapIndexed((i, col) {
+        double leftPadding = 0;
+        double rightPadding = 0;
+
+        if (!values.first.columns.keys.elementAt(i).isRhytmic ||
+            i == columnColumns.length - 1) {
+          rightPadding = layoutProperties.staveSpace;
+        }
+        if (i == 0) {
+          leftPadding = layoutProperties.staveSpace;
+        }
+        return _MeasureColumn(
+          columns: col,
+          padding: EdgeInsets.only(left: leftPadding, right: rightPadding),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class _MeasureColumn extends StatelessWidget {
+  final EdgeInsets? padding;
+  final List<MeasureGridColumn> columns;
+
+  const _MeasureColumn({
+    super.key,
+    required this.columns,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var flattend = columns.expand((i) => i.values.entries).toList();
+
+    NotationLayoutProperties layoutProperties =
+        NotationProperties.of(context)?.layout ??
+            NotationLayoutProperties.standard();
+
+    double spacePerPosition = layoutProperties.spacePerPosition;
+    ElementPosition bottomRef = columns.first.values.keys.last;
+    ElementPosition topRef = columns.first.values.keys.first;
+    const staveRef = ElementPosition(step: Step.F, octave: 4);
+
+    double width = 0;
+    for (var cell in flattend) {
+      if (cell.value != null) {
+        width = max(width, cell.value!.size.width);
+      }
+    }
+    width += padding?.horizontal ?? 0;
+
+    return SizedBox(
+      height: columns.first.values.length * spacePerPosition,
+      width: width,
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          Positioned(
+            bottom: staveRef.distance(bottomRef) * spacePerPosition,
+            child: StaffLines(),
+          ),
+          ...flattend.where((cell) => cell.value != null).map((cell) {
+            var element = cell.value!;
+
+            ElementPosition pos = element.position;
+            AlignmentPosition alignment = element.alignmentPosition;
+            double? top;
+            double? bottom;
+
+            if (alignment.top == null) {
+              bottom = -alignment.bottom!;
+              bottom += pos.distance(bottomRef) * spacePerPosition;
+            } else {
+              top = alignment.top!;
+              top += topRef.distance(pos) * spacePerPosition;
+            }
+
+            return Positioned(
+              left: padding?.left ?? 0,
+              top: top,
+              bottom: bottom,
+              child: element,
+            );
+          })
+        ],
+      ),
     );
   }
 }
