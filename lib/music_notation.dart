0010385c1817ee47faf5cd99b4287187e82fac12
ViliusP <viliuspaliokas@gmail.com>
2024-12-05 15:19:00 +0200

    Cleaning

--- lib/music_notation.dart
+++ lib/music_notation.dart
@@ -8,8 +8,6 @@ export 'src/notation_painter/properties/notation_properties.dart'
 
 export 'src/notation_painter/music_sheet/music_sheet.dart';
 
-export 'src/notation_painter/sync_width_column.dart' show SyncWidthColumn;
-
 export 'src/models/elements/score/score.dart' show ScorePartwise, ScoreHeader;
 
 export 'src/smufl/glyph_class.dart' show GlyphClasses;
