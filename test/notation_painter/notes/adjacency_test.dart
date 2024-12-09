bd936be1656f2847139298c0054b7de128a1a6e5
ViliusP <viliuspaliokas@gmail.com>
2024-12-09 21:36:59 +0200

    Fix tests

--- test/notation_painter/notes/adjacency_test.dart
+++ test/notation_painter/notes/adjacency_test.dart
@@ -1,5 +1,5 @@
 import 'package:music_notation/src/models/data_types/step.dart';
-import 'package:music_notation/src/models/elements/music_data/note/note.dart';
+import 'package:music_notation/src/notation_painter/models/element_position.dart';
 import 'package:music_notation/src/notation_painter/notes/adjacency.dart';
 import 'package:music_notation/src/notation_painter/notes/stemming.dart';
 import 'package:test/test.dart';
@@ -10,68 +10,68 @@ void main() {
     () {
       test("Two notes #1 (F4, G4)", () {
         final testCase = (
-          notes: [
-            RegularNote(form: Pitch(step: Step.F, octave: 4), duration: 0),
-            RegularNote(form: Pitch(step: Step.G, octave: 4), duration: 0),
+          positions: [
+            ElementPosition(step: Step.F, octave: 4),
+            ElementPosition(step: Step.G, octave: 4),
           ],
           stemDirection: StemDirection.down,
-          expected: [NoteheadPosition.left, NoteheadPosition.right],
+          expected: [NoteheadSide.left, NoteheadSide.right],
         );
 
-        String notesDescription = testCase.notes
-            .map((note) => "${note.form.step}${note.form.octave}")
+        String notesDescription = testCase.positions
+            .map((note) => "${note.step}${note.octave}")
             .join(", ");
         notesDescription = "($notesDescription)";
 
-        var result = Adjacency.determineNoteheadPositions(
-          testCase.notes,
+        var result = Adjacency.determineNoteSides(
+          testCase.positions,
           testCase.stemDirection,
         );
         expect(result, orderedEquals(testCase.expected));
       });
       test("Two notes #1 (E5, F5)", () {
         final testCase = (
-          notes: [
-            RegularNote(form: Pitch(step: Step.E, octave: 5), duration: 0),
-            RegularNote(form: Pitch(step: Step.F, octave: 5), duration: 0),
+          positions: [
+            ElementPosition(step: Step.E, octave: 5),
+            ElementPosition(step: Step.F, octave: 5),
           ],
           stemDirection: StemDirection.down,
-          expected: [NoteheadPosition.left, NoteheadPosition.right],
+          expected: [NoteheadSide.left, NoteheadSide.right],
         );
 
-        String notesDescription = testCase.notes
-            .map((note) => "${note.form.step}${note.form.octave}")
+        String notesDescription = testCase.positions
+            .map((note) => "${note.step}${note.octave}")
             .join(", ");
         notesDescription = "($notesDescription)";
 
-        var result = Adjacency.determineNoteheadPositions(
-          testCase.notes,
+        var result = Adjacency.determineNoteSides(
+          testCase.positions,
           testCase.stemDirection,
         );
         expect(result, orderedEquals(testCase.expected));
       });
       test("Odd number (3) of notes, below middle (F4, G4, A4)", () {
         final testCase = (
-          notes: [
-            RegularNote(form: Pitch(step: Step.F, octave: 4), duration: 0),
-            RegularNote(form: Pitch(step: Step.G, octave: 4), duration: 0),
-            RegularNote(form: Pitch(step: Step.A, octave: 4), duration: 0),
+          positions: [
+            ElementPosition(step: Step.F, octave: 4),
+            ElementPosition(step: Step.G, octave: 4),
+            ElementPosition(step: Step.A, octave: 4),
           ],
           stemDirection: StemDirection.up,
           expected: [
-            NoteheadPosition.left,
-            NoteheadPosition.right,
-            NoteheadPosition.left,
+            NoteheadSide.left,
+            NoteheadSide.right,
+            NoteheadSide.left,
           ],
         );
 
-        String notesDescription = testCase.notes
-            .map((note) => "${note.form.step}${note.form.octave}")
+        String notesDescription = testCase.positions
+            .map((note) => "${note.step}${note.octave}")
             .join(", ");
         notesDescription = "($notesDescription)";
 
-        var result = Adjacency.determineNoteheadPositions(
-          testCase.notes,
+        var result = Adjacency.determineNoteSides(
+          testCase.positions,
           testCase.stemDirection,
         );
         expect(result, orderedEquals(testCase.expected));
@@ -80,34 +80,34 @@ void main() {
         "Odd number (7) of notes, below the middle (E4, F4, G4, A4, B4, C5, D5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.E, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.F, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.G, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.A, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.B, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.C, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.D, octave: 5), duration: 0),
+            positions: [
+              ElementPosition(step: Step.E, octave: 4),
+              ElementPosition(step: Step.F, octave: 4),
+              ElementPosition(step: Step.G, octave: 4),
+              ElementPosition(step: Step.A, octave: 4),
+              ElementPosition(step: Step.B, octave: 4),
+              ElementPosition(step: Step.C, octave: 5),
+              ElementPosition(step: Step.D, octave: 5),
             ],
             stemDirection: StemDirection.up,
             expected: [
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
@@ -117,26 +117,26 @@ void main() {
         "Odd number (3) of notes, above the middle (C5, D5, E5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.C, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.D, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.E, octave: 5), duration: 0),
+            positions: [
+              ElementPosition(step: Step.C, octave: 5),
+              ElementPosition(step: Step.D, octave: 5),
+              ElementPosition(step: Step.E, octave: 5),
             ],
             stemDirection: StemDirection.down,
             expected: [
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
@@ -146,30 +146,30 @@ void main() {
         "Odd number (5) of notes, above the middle (D5, E5, F5, G5, A5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.D, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.E, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.F, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.G, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.A, octave: 5), duration: 0),
+            positions: [
+              ElementPosition(step: Step.D, octave: 5),
+              ElementPosition(step: Step.E, octave: 5),
+              ElementPosition(step: Step.F, octave: 5),
+              ElementPosition(step: Step.G, octave: 5),
+              ElementPosition(step: Step.A, octave: 5),
             ],
             stemDirection: StemDirection.down,
             expected: [
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
@@ -179,28 +179,28 @@ void main() {
         "Even number (4) of notes, below the middle (F5, G5, A5, B5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.F, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.G, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.A, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.B, octave: 4), duration: 0),
+            positions: [
+              ElementPosition(step: Step.F, octave: 4),
+              ElementPosition(step: Step.G, octave: 4),
+              ElementPosition(step: Step.A, octave: 4),
+              ElementPosition(step: Step.B, octave: 4),
             ],
             stemDirection: StemDirection.up,
             expected: [
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
@@ -211,32 +211,32 @@ void main() {
         "Even number (6) of notes, above the middle (D5, E5, F5, G5, A5, B5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.D, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.E, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.F, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.G, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.A, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.B, octave: 5), duration: 0),
+            positions: [
+              ElementPosition(step: Step.D, octave: 5),
+              ElementPosition(step: Step.E, octave: 5),
+              ElementPosition(step: Step.F, octave: 5),
+              ElementPosition(step: Step.G, octave: 5),
+              ElementPosition(step: Step.A, octave: 5),
+              ElementPosition(step: Step.B, octave: 5),
             ],
             stemDirection: StemDirection.down,
             expected: [
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
@@ -247,28 +247,28 @@ void main() {
         "Adjacent notes in multiple groups #1 (A4, B4, F5, G5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.A, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.B, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.F, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.G, octave: 5), duration: 0),
+            positions: [
+              ElementPosition(step: Step.A, octave: 4),
+              ElementPosition(step: Step.B, octave: 4),
+              ElementPosition(step: Step.F, octave: 5),
+              ElementPosition(step: Step.G, octave: 5),
             ],
             stemDirection: StemDirection.down,
             expected: [
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
@@ -278,28 +278,28 @@ void main() {
         "Adjacent notes in multiple groups #1 (G4, A4, C5, D5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.G, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.A, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.C, octave: 5), duration: 0),
-              RegularNote(form: Pitch(step: Step.D, octave: 5), duration: 0),
+            positions: [
+              ElementPosition(step: Step.G, octave: 4),
+              ElementPosition(step: Step.A, octave: 4),
+              ElementPosition(step: Step.C, octave: 5),
+              ElementPosition(step: Step.D, octave: 5),
             ],
             stemDirection: StemDirection.up,
             expected: [
-              NoteheadPosition.left,
-              NoteheadPosition.right,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
+              NoteheadSide.left,
+              NoteheadSide.right,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
@@ -309,26 +309,26 @@ void main() {
         "Adjacent notes in multiple groups #3 (E4, G4, A5)",
         () {
           final testCase = (
-            notes: [
-              RegularNote(form: Pitch(step: Step.E, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.G, octave: 4), duration: 0),
-              RegularNote(form: Pitch(step: Step.A, octave: 4), duration: 0),
+            positions: [
+              ElementPosition(step: Step.E, octave: 4),
+              ElementPosition(step: Step.G, octave: 4),
+              ElementPosition(step: Step.A, octave: 4),
             ],
             stemDirection: StemDirection.up,
             expected: [
-              NoteheadPosition.left,
-              NoteheadPosition.left,
-              NoteheadPosition.right,
+              NoteheadSide.left,
+              NoteheadSide.left,
+              NoteheadSide.right,
             ],
           );
 
-          String notesDescription = testCase.notes
-              .map((note) => "${note.form.step}${note.form.octave}")
+          String notesDescription = testCase.positions
+              .map((note) => "${note.step}${note.octave}")
               .join(", ");
           notesDescription = "($notesDescription)";
 
-          var result = Adjacency.determineNoteheadPositions(
-            testCase.notes,
+          var result = Adjacency.determineNoteSides(
+            testCase.positions,
             testCase.stemDirection,
           );
           expect(result, orderedEquals(testCase.expected));
