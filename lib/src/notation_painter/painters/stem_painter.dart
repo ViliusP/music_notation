c561338054db4b4700bc43dfd2d1ccc482e16d52
ViliusP <viliuspaliokas@gmail.com>
2024-12-10 13:22:55 +0200

    Change stroke cap of StemPainter to butt

--- lib/src/notation_painter/painters/stem_painter.dart
+++ lib/src/notation_painter/painters/stem_painter.dart
@@ -15,7 +15,7 @@ class StemPainter extends CustomPainter {
     final Paint paint = Paint()
       ..color = const Color.fromRGBO(0, 0, 0, 1.0)
       ..strokeWidth = thickness
-      ..strokeCap = StrokeCap.round;
+      ..strokeCap = StrokeCap.butt;
 
     canvas.drawLine(Offset.zero, Offset(0, size.height), paint);
   }
