718f9c78bf6534bcdaafedc62830d3fa975e210d
ViliusP <viliuspaliokas@gmail.com>
2024-12-18 00:16:49 +0200

    Fix RestElement

--- lib/src/notation_painter/notes/rest_element.dart
+++ lib/src/notation_painter/notes/rest_element.dart
@@ -4,7 +4,7 @@ import 'package:music_notation/src/models/data_types/step.dart';
 import 'package:music_notation/src/models/elements/music_data/attributes/clef.dart';
 import 'package:music_notation/src/models/elements/music_data/note/note.dart';
 import 'package:music_notation/src/models/elements/music_data/note/note_type.dart';
-import 'package:music_notation/src/notation_painter/layout/measure_element.dart';
+import 'package:music_notation/src/notation_painter/layout/positioning.dart';
 import 'package:music_notation/src/notation_painter/models/element_position.dart';
 import 'package:music_notation/src/notation_painter/notes/augmentation_dots.dart';
 import 'package:music_notation/src/notation_painter/notes/note_element.dart';
@@ -23,70 +23,6 @@ class RestElement extends StatelessWidget {
   final FontMetadata font;
 
   final String? voice;
-
-  AlignmentOffset get offset {
-    return AlignmentOffset.fromTop(
-      left: 0,
-      height: size.height,
-      top: -_verticalAlignmentAxisOffset,
-    );
-  }
-
-  GlyphBBox get _bBox {
-    return font.glyphBBoxes[_glyph]!;
-  }
-
-  double get _verticalAlignmentAxisOffset {
-    double alignment = _bBox.bBoxNE.y;
-    if (dots != null) {
-      double maybeDotOffset = alignment;
-      maybeDotOffset -= dots?.offset.top ?? 0;
-      maybeDotOffset -= 1 / 2;
-      if (maybeDotOffset < 0) {
-        alignment -= maybeDotOffset;
-      }
-    }
-    return alignment;
-  }
-
-  final bool isMeasure;
-
-  Size get size {
-    Size restSymbolSize = _bBox.toSize();
-    double width = restSymbolSize.width;
-    double height = restSymbolSize.height;
-    if (dots != null) {
-      width += AugmentationDots.defaultBaseOffset;
-      width += dots!.size.width;
-
-      if (_dotsVerticalOffset < 0) {
-        height += _dotsVerticalOffset.abs();
-      }
-    }
-    return Size(width, height);
-  }
-
-  double get _dotsVerticalOffset {
-    if (dots == null) return 0;
-    // Alignment line of rest.
-    double top = _bBox.bBoxNE.y;
-
-    top -= dots!.offset.top;
-    top -= .5;
-    return top;
-  }
-
-  static NoteTypeValue _determineType(Note note) {
-    NoteTypeValue? type = note.type?.value;
-
-    NoteForm noteForm = note.form;
-    if ((noteForm as Rest).measure == true) {
-      type = NoteTypeValue.whole;
-    }
-    return type ?? NoteTypeValue.quarter;
-  }
-
-  SmuflGlyph get _glyph => _determineGlyph(type, position);
 
   factory RestElement.fromNote({
     required Note note,
@@ -117,49 +53,96 @@ class RestElement extends StatelessWidget {
     this.isMeasure = false,
   });
 
-  static SmuflGlyph _determineGlyph(NoteTypeValue type, ElementPosition pos) {
-    switch (type) {
-      case NoteTypeValue.n1024th:
-        return SmuflGlyph.rest1024th;
-      case NoteTypeValue.n512th:
-        return SmuflGlyph.rest512th;
-      case NoteTypeValue.n256th:
-        return SmuflGlyph.rest256th;
-      case NoteTypeValue.n128th:
-        return SmuflGlyph.rest128th;
-      case NoteTypeValue.n64th:
-        return SmuflGlyph.rest64th;
-      case NoteTypeValue.n32nd:
-        return SmuflGlyph.rest32nd;
-      case NoteTypeValue.n16th:
-        return SmuflGlyph.rest16th;
-      case NoteTypeValue.eighth:
-        return SmuflGlyph.rest8th;
-      case NoteTypeValue.quarter:
-        return SmuflGlyph.restQuarter;
-      case NoteTypeValue.half:
-        if (pos.numeric >= ElementPosition.firstLedgerAbove.numeric) {
-          return SmuflGlyph.restHalfLegerLine;
-        }
-        if (pos.numeric <= ElementPosition.secondLedgerBelow.numeric) {
-          return SmuflGlyph.restHalfLegerLine;
-        }
-        return SmuflGlyph.restHalf;
-      case NoteTypeValue.whole:
-        if (pos.numeric >= ElementPosition.firstLedgerAbove.numeric) {
-          return SmuflGlyph.restWholeLegerLine;
-        }
-        if (pos.numeric <= ElementPosition.secondLedgerBelow.numeric) {
-          return SmuflGlyph.restWholeLegerLine;
-        }
-        return SmuflGlyph.restWhole;
-      case NoteTypeValue.breve:
-        return SmuflGlyph.restDoubleWhole;
-      case NoteTypeValue.long:
-        return SmuflGlyph.restLonga;
-      case NoteTypeValue.maxima:
-        return SmuflGlyph.restMaxima;
+  Alignment get alignment {
+    if (dots == null) return _bBox.toAlignment();
+
+    var range = [
+      MeasureElementLayoutData(
+        size: dots!.size,
+        position: position + _dotsPositionIncrease,
+        alignment: dots!.alignment,
+      ),
+      MeasureElementLayoutData(
+        size: _bBox.toSize(),
+        position: position,
+        alignment: _bBox.toAlignment(),
+      ),
+    ].columnVerticalRange(position);
+
+    return Alignment(
+        0,
+        MeasureElementLayoutData.calculateSingleAxisAlignment(
+          range.min,
+          range.max,
+          Axis.vertical,
+        ));
+  }
+
+  GlyphBBox get _bBox {
+    return font.glyphBBoxes[_glyph]!;
+  }
+
+  int get _dotsPositionIncrease => switch (position.numeric % 2 == 0) {
+        true => 1,
+        false => 0,
+      };
+
+  final bool isMeasure;
+
+  Size get size {
+    Size restSize = _bBox.toSize();
+    if (dots == null) return restSize;
+    Rect dotRect = Offset.zero & dots!.size;
+    dotRect = dotRect.shift(Offset(
+      restSize.width + AugmentationDots.defaultBaseOffset,
+      _dotsVerticalOffset,
+    ));
+
+    return (Offset.zero & restSize).expandToInclude(dotRect).size;
+  }
+
+  double get _dotsVerticalOffset {
+    Size restSize = _bBox.toSize();
+    Rect dotRect = Offset.zero & dots!.size;
+
+    return restSize.height / 2 -
+        dotRect.height / 2 +
+        -_dotsPositionIncrease * 1 / 2;
+  }
+
+  SmuflGlyph get _glyph => _determineGlyph(type, position);
+
+  static NoteTypeValue _determineType(Note note) {
+    NoteTypeValue? type = note.type?.value;
+
+    NoteForm noteForm = note.form;
+    if ((noteForm as Rest).measure == true) {
+      type = NoteTypeValue.whole;
     }
+    return type ?? NoteTypeValue.quarter;
+  }
+
+  static SmuflGlyph _determineGlyph(NoteTypeValue type, ElementPosition pos) {
+    bool hasLedgerLines = !pos.betweenLedgers;
+
+    return switch (type) {
+      NoteTypeValue.n1024th => SmuflGlyph.rest1024th,
+      NoteTypeValue.n512th => SmuflGlyph.rest512th,
+      NoteTypeValue.n256th => SmuflGlyph.rest256th,
+      NoteTypeValue.n128th => SmuflGlyph.rest128th,
+      NoteTypeValue.n64th => SmuflGlyph.rest64th,
+      NoteTypeValue.n32nd => SmuflGlyph.rest32nd,
+      NoteTypeValue.n16th => SmuflGlyph.rest16th,
+      NoteTypeValue.eighth => SmuflGlyph.rest8th,
+      NoteTypeValue.quarter => SmuflGlyph.restQuarter,
+      NoteTypeValue.half when hasLedgerLines => SmuflGlyph.restHalfLegerLine,
+      NoteTypeValue.half => SmuflGlyph.restHalf,
+      NoteTypeValue.whole when hasLedgerLines => SmuflGlyph.restWholeLegerLine,
+      NoteTypeValue.whole => SmuflGlyph.restWhole,
+      NoteTypeValue.breve => SmuflGlyph.restDoubleWhole,
+      NoteTypeValue.long => SmuflGlyph.restLonga,
+      NoteTypeValue.maxima => SmuflGlyph.restMaxima,
+    };
   }
 
   static ElementPosition _determinePosition(Note note, Clef? clef) {
